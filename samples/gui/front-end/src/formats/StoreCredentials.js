ddb33a5cdc7c249090596429ae56edd26c96be90
SHLOMITK@il.ibm.com <shlomitk@il.ibm.com>
2020-10-13 19:42:10 +0300

    ownerId instead of ownerID

--- samples/gui/front-end/src/formats/StoreCredentials.js
+++ samples/gui/front-end/src/formats/StoreCredentials.js
@@ -45,7 +45,7 @@ const StoreCredentials = props => {
   const handleStore = (event, uid) => {
     creds.forEach(cred => {
       if (cred.uid === uid) {
-        const credentials = array.omitBy({ username: cred.userName, password: cred.password, ownerID: cred.userID }, array.isEmpty)
+        const credentials = array.omitBy({ username: cred.userName, password: cred.password, ownerId: cred.userID }, array.isEmpty)
         axios({
           method: 'post',
           url: process.env.REACT_APP_BACKEND_ADDRESS + '/v1/creds/usercredentials',
