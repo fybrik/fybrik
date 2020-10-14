ba01bf8bc7023a9baea8d190afe6d35fc1db03d0
SHLOMITK@il.ibm.com <shlomitk@il.ibm.com>
2020-10-14 14:46:32 +0300

    remove hard-coded namespace
    Signed-off-by: rohithdv <rovallam@in.ibm.com>

--- samples/gui/deploy.sh
+++ samples/gui/deploy.sh
@@ -2,16 +2,55 @@
 # Copyright 2020 IBM Corp.
 # SPDX-License-Identifier: Apache-2.0
 
+echo "kind: Deployment
+apiVersion: apps/v1
+metadata:
+  name: gui
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: gui
+  template:
+    metadata:
+      labels:
+        app: gui
+    spec:
+      containers:
+      - name: datauserclient
+        image: image-registry.openshift-image-registry.svc:5000/"$KUBE_NAMESPACE"/datauserclient:latest
+        imagePullPolicy: Always
+        ports:
+        - containerPort: 3000
+      - name: datauserserver
+        image: image-registry.openshift-image-registry.svc:5000/"$KUBE_NAMESPACE"/datauserserver:latest
+        imagePullPolicy: Always
+        envFrom:
+        - configMapRef:
+            name: m4d-config
+        env:
+        - name: VAULT_TOKEN
+          valueFrom:
+            secretKeyRef:
+              name: gui-vault-secret
+              key: vault-root  
+        - name: GEOGRAPHY
+          value: US          
+        ports:
+        - containerPort: 8080
+      restartPolicy: Always" > Deployment.yaml
+
 export VAULT_TOKEN=$(kubectl get secrets vault-unseal-keys -n m4d-system -o jsonpath={.data.vault-root} | base64 --decode)
 echo -n $VAULT_TOKEN > ./token.txt
 echo $VAULT_TOKEN
-kubectl apply -f ../../manager/config/prod/deployment_configmap.yaml
-kubectl delete secret vault-unseal-keys -n default
-kubectl create secret generic vault-unseal-keys --from-file=vault-root=./token.txt -n default || true
+kubectl apply -f ../../manager/config/prod/deployment_configmap.yaml 
+kubectl delete secret gui-vault-secret
+kubectl create secret generic gui-vault-secret --from-file=vault-root=./token.txt || true
 kubectl delete service datauserserver || true
 kubectl delete service datauserclient || true
-kubectl delete deployment gui -n default
-kubectl apply -f Deployment.yaml -n default
+kubectl delete deployment gui
+kubectl apply -f Deployment.yaml
+kubectl apply -f resources.yaml
 rm ./token.txt
 
 
