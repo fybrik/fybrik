89f0eeace71e10f4464700a0c60be94a83ef6e67
rohithdv <rovallam@in.ibm.com>
2020-10-18 22:53:00 +0530

    removed unnecessary go.mod files as per comment in PR 31 (https://github.com/IBM/the-mesh-for-data/pull/31#discussion_r498205037). Also updating the user_vault_init to have latest catalog working id

--- connectors/vault/user_vault_init.sh
+++ connectors/vault/user_vault_init.sh
@@ -22,7 +22,7 @@ source $DIR_ROOT/pkg/policy-compiler/policy-compiler.env
 
 export VAULT_BIN=$DIR_ABSTOOLBIN/vault
 #export USER_VAULT_KAFKA_ASSET_KEY=87ffdca3-8b5d-4f77-99f9-0cb1fba1f73f/01c6f0f0-9ffe-4ccc-ac07-409523755e72
-export USER_VAULT_KAFKA_ASSET_KEY="{\"catalog_id\":\"87ffdca3-8b5d-4f77-99f9-0cb1fba1f73f\",\"asset_id\":\"01c6f0f0-9ffe-4ccc-ac07-409523755e72\"}"
+export USER_VAULT_KAFKA_ASSET_KEY="{\"catalog_id\":\"fb4e5fc8-266c-4cf7-a29f-33e3c9d5d091\",\"asset_id\":\"05df45f2-5927-4534-bdff-0d4b923baeea\"}"
 export USER_VAULT_KAFKA_ASSET_CRED="my_kafka_credentials"
 
 export USER_VAULT_EGERIA_ASSET_KEY="{\"ServerName\":\"cocoMDS3\",\"AssetGuid\":\"4098e18e-bd53-4fd0-8ff8-e1c8e9fc42da\"}"
