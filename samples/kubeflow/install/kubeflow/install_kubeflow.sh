5c1428f62bcea906add480db64a2bd41c3a3a2ee
roee88 <roee88@gmail.com>
2020-11-01 11:55:31 +0200

    samples: Fixed Kubeflow installation script on Mac
    Support for downloading the Mac version of kfctl
    
    Signed-off-by: roee88 <roee88@gmail.com>

--- samples/kubeflow/install/kubeflow/install_kubeflow.sh
+++ samples/kubeflow/install/kubeflow/install_kubeflow.sh
@@ -2,10 +2,15 @@
 
 set -e
 
+os="linux"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  os="darwin"
+fi
+
 if [[ ! -f ./kfctl ]]; then
-  wget https://github.com/kubeflow/kfctl/releases/download/v1.0.2/kfctl_v1.0.2-0-ga476281_linux.tar.gz
-  tar -zxvf kfctl_v1.0.2-0-ga476281_linux.tar.gz
-  rm kfctl_v1.0.2-0-ga476281_linux.tar.gz
+  wget https://github.com/kubeflow/kfctl/releases/download/v1.0.2/kfctl_v1.0.2-0-ga476281_${os}.tar.gz
+  tar -zxvf kfctl_v1.0.2-0-ga476281_${os}.tar.gz
+  rm kfctl_v1.0.2-0-ga476281_${os}.tar.gz
 fi
 
 cp kubeflow.yaml kfctl_k8s_istio.v1.0.2.yaml
