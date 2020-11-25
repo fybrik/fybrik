44e36b3f39788517003dfcf58005dc48e5a1c00f
Florian Froese <71638048+froesef@users.noreply.github.com>
2020-11-25 18:54:33 +0100

    Multicluster: Razee installation on kind (#148)
    * multicluster: Initial commit of a razee installation for kind
    
    Signed-off-by: Florian Froese <ffr@zurich.ibm.com>
    
    * multicluster: improve kind cluster setup for os x
    
    Signed-off-by: Florian Froese <ffr@zurich.ibm.com>
    
    * Move razee to third_party folder and add make targets
    
    Signed-off-by: Florian Froese <ffr@zurich.ibm.com>
    
    * razee: Use local authentication for kind
    
    Signed-off-by: Florian Froese <ffr@zurich.ibm.com>
    
    * razee: Extract variable for username and password
    
    Signed-off-by: Florian Froese <ffr@zurich.ibm.com>
    
    * Rename dev CA certificate and remove cert for ubuntu functionality
    
    Signed-off-by: Florian Froese <ffr@zurich.ibm.com>
    
    * Add --fresh to call certificate update call
    
    Signed-off-by: Florian Froese <ffr@zurich.ibm.com>
    
    Co-authored-by: Roee Shlomo <roee88@gmail.com>

--- hack/tools/create_kind.sh
+++ hack/tools/create_kind.sh
@@ -30,30 +30,49 @@ registry_create() {
 }
 
 certs_create() {
-    mkdir ../registry -p || true
-    openssl genrsa -out ../registry/ca.key 2048
-    openssl req -new -x509 -key ../registry/ca.key -out ../registry/ca.crt -subj '/C=US/ST=NY/O=IBM/CN=ibm' -extensions EXT -config <(printf "[dn]\nCN=ibm\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:ibm\nbasicConstraints=CA:TRUE,pathlen:0")
-    openssl genrsa -out ../registry/registry.key 2048
-    openssl req -new -key ../registry/registry.key -out ../registry/registry.csr -subj '/C=US/ST=NY/O=IBM/CN=kind-registry' -extensions EXT -config <(printf "[dn]\nCN=kind-registry\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:kind-registry,DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
-    openssl x509 -req -in ../registry/registry.csr -CA ../registry/ca.crt -CAkey ../registry/ca.key -CAcreateserial -out ../registry/registry.crt
+    mkdir -p ../registry || true
+
+    if [ ! -f ../registry/themeshfordata-ca.crt ]; then
+      openssl genrsa -out ../registry/themeshfordata-ca.key 2048
+      openssl req -new -x509 -key ../registry/themeshfordata-ca.key -out ../registry/themeshfordata-ca.crt -subj '/C=US/ST=NY/O=IBM/CN=themeshfordata' -extensions EXT -config <(printf "[dn]\nCN=ibm\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:ibm\nbasicConstraints=CA:TRUE,pathlen:0")
+    fi
+    if [ ! -f ../registry/registry.crt ]; then
+      openssl genrsa -out ../registry/registry.key 2048
+      openssl req -new -key ../registry/registry.key -out ../registry/registry.csr -subj '/C=US/ST=NY/O=IBM/CN=kind-registry' -extensions EXT -config <(printf "[dn]\nCN=kind-registry\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:kind-registry,DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+      openssl x509 -req -in ../registry/registry.csr -CA ../registry/themeshfordata-ca.crt -CAkey ../registry/themeshfordata-ca.key -CAcreateserial -out ../registry/registry.crt
+    fi
 }
 
 install_certs() {
     if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        sudo cp ../registry/ca.crt /usr/local/share/ca-certificates
-	      sudo update-ca-certificates
+        # This has been tested on Ubuntu. On other distributions this may vary.
+        # If developers use different distributions please add a branch here that works.
+        sudo cp ../registry/themeshfordata-ca.crt /usr/local/share/ca-certificates
+	      sudo update-ca-certificates --fresh
 	  elif [[ "$OSTYPE" == "darwin"* ]]; then
-        echo OSX will ask to provide your password in order to install the CA certificate to keychain!
-        # Installs the CA certificate to the user local keychain
-        security add-trusted-cert -r trustRoot -k ~/Library/Keychains/login.keychain-db -e hostnameMismatch ../registry/ca.crt
+	      VALIDCERT=0
+	      security verify-cert -k /Users/ffr/Library/Keychains/login.keychain-db -c ../registry/registry.crt || VALIDCERT=$?
+        if [ $VALIDCERT -eq 0 ]; then
+          echo Certificate already valid!
+        else
+          echo OSX will ask to provide your password in order to install the CA certificate to keychain!
+          # Installs the CA certificate to the user local keychain
+          security add-trusted-cert -r trustRoot -k ~/Library/Keychains/login.keychain-db -e hostnameMismatch ../registry/themeshfordata-ca.crt
+        fi
     else
-        echo Please install the certificates in $PWD/../registry/ca.crt !
+        echo Please install the certificates in $PWD/../registry/themeshfordata-ca.crt !
     fi
 }
 
 certs_delete() {
-    if [[ "$OSTYPE" == "darwin"* ]]; then
-      security remove-trusted-cert  ../registry/ca.crt
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+      # This has been tested on Ubuntu. On other distributions this may vary.
+      # If developers use different distributions please add a branch here that works.
+      sudo rm /usr/local/share/ca-certificates/themeshfordata-ca.crt
+      sudo update-ca-certificates --fresh
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+      security remove-trusted-cert  ../registry/themeshfordata-ca.crt
+      security delete-certificate -c themeshfordata
     fi
     rm -rf ../registry
 }
@@ -67,10 +86,9 @@ kind_create() {
              -v 4 --retain --wait=0s \
              --config ./$2 \
              --image=kindest/node:$K8S_VERSION
-        bin/kubectl config use-context kind-$1
         for node in $(kind get nodes --name $1); do
-          bin/kubectl annotate node "${node}" "tilt.dev/registry=kind-registry:5000";
-          docker cp ../registry/ca.crt "$node":/usr/local/share/ca-certificates
+          bin/kubectl annotate node "${node}" "tilt.dev/registry=kind-registry:5000" --context kind-${1};
+          docker cp ../registry/themeshfordata-ca.crt "$node":/usr/local/share/ca-certificates
           docker exec "$node" update-ca-certificates
         done
 }
@@ -79,7 +97,7 @@ case "$op" in
     cleanup)
         header_text "Uninstalling kind cluster"
         registry_delete || true
-        certs_delete || true
+        # certs_delete || true # Deactivated for now to simplify development when recreating a kind cluster
         kind_delete kind || true
         kind_delete control || true
         ;;
@@ -87,9 +105,10 @@ case "$op" in
         header_text "Installing kind multi-cluster"
         certs_create
         install_certs
-        kind_create kind kind-config.yaml
+        kind_create kind kind-config.yaml &
+        kind_create control kind-control-config.yaml &
+        wait
         registry_create
-        kind_create control kind-control-config.yaml
         ;;
     *)
         header_text "Installing kind cluster"
