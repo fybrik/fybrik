3884a022b7cc418226d3cf2d2f0e2290f4aa91ce
Florian Froese <ffr@zurich.ibm.com>
2021-01-12 19:38:34 +0100

    Improve LocalAuthClient to invalidate token
    Signed-off-by: Florian Froese <ffr@zurich.ibm.com>

--- pkg/multicluster/razee/razee.go
+++ pkg/multicluster/razee/razee.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/machinebox/graphql"
 	"net/http"
+	"time"
 )
 
 //nolint:golint,unused
@@ -17,18 +18,21 @@ func (t *RazeeGithubApiRoundTripper) RoundTrip(request *http.Request) (*http.Res
 	return http.DefaultTransport.RoundTrip(request)
 }
 
+const TokenValidityDuration = 40 * time.Minute
+
 //nolint:golint,unused
 type LocalAuthClient struct {
 	url      string
 	login    string
 	password string
 	token    string
+	tokenTimestamp time.Time
 }
 
 // Authentication method used by sat-con-client library
 //nolint:golint,unused
 func (t *LocalAuthClient) Authenticate(request *http.Request) error {
-	if t.token == "" {
+	if t.token == "" || time.Since(t.tokenTimestamp) >= TokenValidityDuration {
 		req := graphql.NewRequest(`
 		mutation ($login: String!, $password: String!) {
 		  signIn(
@@ -58,6 +62,6 @@ func (t *LocalAuthClient) Authenticate(request *http.Request) error {
 		t.token = result.Signin.Token
 	}
 
-	request.Header.Add("Authorization", "Bearer "+t.token)
+	request.Header.Add("Authorization", "Bearer " + t.token)
 	return nil
 }
