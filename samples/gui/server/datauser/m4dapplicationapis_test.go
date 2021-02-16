8e42936b2d4474d7a436cdc2a2dea87078a68674
SHLOMITK@il.ibm.com <shlomitk@il.ibm.com>
2021-02-16 13:14:31 +0200

    adding a test
    Signed-off-by: SHLOMITK@il.ibm.com <shlomitk@il.ibm.com>

--- samples/gui/server/datauser/m4dapplicationapis_test.go
+++ samples/gui/server/datauser/m4dapplicationapis_test.go
@@ -8,119 +8,64 @@ import (
 	"strings"
 	"testing"
 
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
-	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
+	"github.com/stretchr/testify/assert"
 )
 
-var _ = Describe("Data User REST API server", func() {
+var (
+	dmaserverurl = "http://localhost:8080/v1/dma/m4dapplication"
+	dm1          = "{\"apiVersion\": \"app.m4d.ibm.com/v1alpha1\",\"kind\": \"M4DApplication\",\"metadata\": {\"name\": \"unittest-read\"},\"spec\": {\"selector\": {\"workloadSelector\": {\"matchLabels\":{\"app\": \"notebook\"}}},\"appInfo\": {\"purpose\": \"fraud-detection\",\"role\": \"Security\"}, \"data\": [{\"dataSetID\": \"123\",\"requirements\": { \"interface\": {\"protocol\": \"s3\",\"dataformat\": \"parquet\"}}}]}}"
+	dm1name      = "unittest-read"
+	dm2          = "{\"apiVersion\": \"app.m4d.ibm.com/v1alpha1\",\"kind\": \"M4DApplication\",\"metadata\": {\"name\": \"unittest-copy\"},\"spec\": {\"selector\": {\"workloadSelector\": {}},\"appInfo\": {\"purpose\": \"copy data\",\"role\": \"data owner\"}, \"data\": [{\"dataSetID\": \"456\",\"requirements\": {\"copy\": {\"required\": true,\"catalog\": {\"catalogID\": \"enterprise\"}}, \"interface\": {\"protocol\": \"s3\",\"dataformat\": \"parquet\"}}}]}}"
+	dm2name      = "unittest-copy"
+)
 
-	BeforeEach(func() {
-		// Add any setup steps that needs to be executed before each test
+func createApplication(t *testing.T, obj string, name string) {
+	body := strings.NewReader(obj)
+	req, err := http.NewRequest("POST", dmaserverurl, body)
+	assert.Nil(t, err)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	resp, err := http.DefaultClient.Do(req)
+	assert.Nil(t, err)
+	assert.Equal(t, resp.StatusCode, http.StatusCreated, "Failed to create application "+name)
+	defer resp.Body.Close()
+}
 
-	})
+func listApplications(t *testing.T) {
+	url := dmaserverurl
+	resp, err := http.Get(url)
+	assert.Nil(t, err)
+	assert.Equal(t, resp.StatusCode, http.StatusOK, "Failed to list applications")
+	defer resp.Body.Close()
+}
 
-	AfterEach(func() {
-		// Add any teardown steps that needs to be executed after each test
-	})
+func getApplication(t *testing.T, name string) {
+	url := dmaserverurl + "/" + name
+	resp, err := http.Get(url)
+	assert.Nil(t, err)
+	assert.Equal(t, resp.StatusCode, http.StatusOK, "Failed to get application "+name)
+	defer resp.Body.Close()
+}
 
-	Context("M4DApplication REST APIs", func() {
-		dmaserverurl := "http://localhost:8080/v1/dma/m4dapplication"
+func deleteApplication(t *testing.T, name string) {
+	url := dmaserverurl + "/" + name
+	req, err := http.NewRequest("DELETE", url, nil)
+	assert.Nil(t, err)
 
-		dm1 := "{\"apiVersion\": \"app.m4d.ibm.com/v1alpha1\",\"kind\": \"M4DApplication\",\"metadata\": {\"name\": \"unittest-notebook1\"},\"spec\": {\"selector\": {\"matchLabels\":{\"app\": \"notebook1\"}},\"appInfo\": {\"purpose\": \"fraud-detection\",\"role\": \"Security\"}, \"data\": [{\"dataSetID\": \"2d1b5352-1fbf-439b-8bb0-c1967ac484b9\",\"ifDetails\": {\"protocol\": \"s3\",\"dataformat\": \"parquet\"}}]}}"
-		dm1name := "unittest-notebook1"
-		dm2 := "{\"apiVersion\": \"app.m4d.ibm.com/v1alpha1\",\"kind\": \"M4DApplication\",\"metadata\": {\"name\": \"unittest-notebook2\"},\"spec\": {\"selector\": {\"matchLabels\":{\"app\": \"notebook2\"}},\"appInfo\": {\"purpose\": \"fraud-detection\",\"role\": \"Security\"}, \"data\": [{\"dataSetID\": \"2d1b5352-1fbf-439b-8bb0-c1967ac484b9\",\"ifDetails\": {\"protocol\": \"s3\",\"dataformat\": \"parquet\"}}]}}"
-		dm2name := "unittest-notebook2"
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
-		It("Create M4DApplication "+dm1name, func() {
-			var err error
+	resp, err := http.DefaultClient.Do(req)
+	assert.Nil(t, err)
+	assert.Equal(t, resp.StatusCode, http.StatusOK, "Failed to delete application "+name)
 
-			// Call the REST API to create a new M4DApplication CRD
-			body := strings.NewReader(dm1)
-			req, err := http.NewRequest("POST", dmaserverurl, body)
-			Expect(err).ToNot(HaveOccurred())
+	defer resp.Body.Close()
+}
 
-			req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-
-			resp, err := http.DefaultClient.Do(req)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(resp.StatusCode).To(Equal(http.StatusCreated))
-
-			defer resp.Body.Close()
-		})
-
-		It("Create M4DApplication "+dm2name, func() {
-			body := strings.NewReader(dm2)
-			req, err := http.NewRequest("POST", dmaserverurl, body)
-			Expect(err).ToNot(HaveOccurred())
-
-			req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-
-			resp, err := http.DefaultClient.Do(req)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(resp.StatusCode).To(Equal(http.StatusCreated))
-
-			defer resp.Body.Close()
-		})
-
-		It("Get a list of all M4DApplications", func() {
-			resp, err := http.Get(dmaserverurl)
-			Expect(err).ToNot(HaveOccurred())
-
-			// TODO - Fix expectations to check results
-			Expect(resp.StatusCode).To(Equal(http.StatusOK))
-
-			defer resp.Body.Close()
-		})
-
-		It("Get a specific M4DApplication - "+dm1name, func() {
-			url := dmaserverurl + "/" + dm1name
-			resp, err := http.Get(url)
-			Expect(err).ToNot(HaveOccurred())
-
-			// TODO - Fix expectations to check results
-			Expect(resp.StatusCode).To(Equal(http.StatusOK))
-
-			defer resp.Body.Close()
-		})
-
-		It("Delete M4DApplication "+dm1name, func() {
-			// Call the REST API to delete an existing M4DApplication CRD
-			url := dmaserverurl + "/" + dm1name
-			req, err := http.NewRequest("DELETE", url, nil)
-			Expect(err).ToNot(HaveOccurred())
-
-			req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-
-			resp, err := http.DefaultClient.Do(req)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(resp.StatusCode).To(Equal(http.StatusOK))
-
-			defer resp.Body.Close()
-		})
-
-		It("Delete M4DApplication "+dm2name, func() {
-			// Call the REST API to delete an existing M4DApplication CRD
-			url := dmaserverurl + "/" + dm2name
-			req, err := http.NewRequest("DELETE", url, nil)
-			Expect(err).ToNot(HaveOccurred())
-
-			req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-
-			resp, err := http.DefaultClient.Do(req)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(resp.StatusCode).To(Equal(http.StatusOK))
-
-			defer resp.Body.Close()
-		})
-
-	})
-})
-
-func TestDMAAPIs(t *testing.T) {
-	RegisterFailHandler(Fail)
-
-	RunSpecsWithDefaultAndCustomReporters(t,
-		"M4DApplication REST API Suite",
-		[]Reporter{printer.NewlineReporter{}})
+func TestApplicationAPIs(t *testing.T) {
+	createApplication(t, dm1, dm1name)
+	createApplication(t, dm2, dm2name)
+	listApplications(t)
+	getApplication(t, dm1name)
+	getApplication(t, dm2name)
+	deleteApplication(t, dm1name)
+	deleteApplication(t, dm2name)
 }
