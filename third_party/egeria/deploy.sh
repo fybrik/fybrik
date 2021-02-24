09bc7b9838179f028055704f6b5e1b124f3b8ad2
rohithdv <rovallam@in.ibm.com>
2021-02-24 16:12:18 +0530

    deploy script changes for egeria 2.6 support
    Signed-off-by: rohithdv <rovallam@in.ibm.com>

--- third_party/egeria/deploy.sh
+++ third_party/egeria/deploy.sh
@@ -3,23 +3,25 @@
 set -e
 set -x
 
-: ${WITHOUT_OPENSHIFT=true}
+: ${WITHOUT_OPENSHIFT=false}
 
-CHART=odpi-egeria-lab
-RELEASE=lab
-NAMESPACE=egeria-catalog
-TIMEOUT=8m
-VERSION="V2.1"
+# CHART=odpi-egeria-lab
+# RELEASE=lab
+NAMESPACE=egeria-catalog2
+TIMEOUT=16m
+VERSION="egeria-release-2.6"
 
 undeploy() {
         # scc permissions for egeria pods to execute
-        # this is done as per guidelines mentioned in https://github.com/odpi/egeria/issues/2790#issuecomment-674879248    
+        # this is done as per guidelines mentioned in https://github.com/odpi/egeria/issues/2790#issuecomment-674879248
         $WITHOUT_OPENSHIFT || oc delete securitycontextconstraints --ignore-not-found egeria-restricted
 
-        helm delete $RELEASE \
-            --namespace=$NAMESPACE \
-            2>/dev/null \
-            || true
+        rm -rf egeria
+        helm delete egeria --namespace=$NAMESPACE --timeout=${TIMEOUT} || true
+        # helm delete $RELEASE \
+        #     --namespace=$NAMESPACE \
+        #     2>/dev/null \
+        #     || true
 }
 
 deploy() {
@@ -37,39 +39,43 @@ deploy() {
         kubectl create ns $NAMESPACE || true
 
         # scc permissions for egeria pods to execute
-        # this is done as per guidelines mentioned in https://github.com/odpi/egeria/issues/2790#issuecomment-674879248 
+        # this is done as per guidelines mentioned in https://github.com/odpi/egeria/issues/2790#issuecomment-674879248
         $WITHOUT_OPENSHIFT || oc create -f egeria-scc.yaml
         $WITHOUT_OPENSHIFT || oc adm policy add-scc-to-user egeria-restricted -z default -n $NAMESPACE
 
-        local chartpath=$(find . -name $CHART | grep charts)
-        helm dep update $chartpath
-        helm install $RELEASE $chartpath \
-            -f lab.yaml \
-            --namespace=$NAMESPACE \
-            --wait --timeout=${TIMEOUT}
+        cd egeria/open-metadata-resources/open-metadata-deployment/charts
+        helm dep update egeria-base
+        helm install egeria egeria-base --namespace=$NAMESPACE --wait --timeout=${TIMEOUT} -f ../../../../lab.yaml
+        cd ../../../../
+
+        # local chartpath=$(find . -name $CHART | grep charts)
+        # helm dep update $chartpath
+        # helm install $RELEASE $chartpath \
+        #     -f lab.yaml \
+        #     --namespace=$NAMESPACE \
+        #     --wait --timeout=${TIMEOUT}
 
         rm -rf egeria
 }
 
-config() {
-        echo "Executing Egaria Lab configuration"
-        local pod=$(kubectl -n $NAMESPACE get pods \
-            -l app.kubernetes.io/component=jupyter,app.kubernetes.io/name=$CHART,app.kubernetes.io/instance=$RELEASE \
-            -o=jsonpath='{.items[0].metadata.name}')
+# config() {
+#         echo "Executing Egaria Lab configuration"
+#         local pod=$(kubectl -n $NAMESPACE get pods \
+#             -l app.kubernetes.io/component=jupyter,app.kubernetes.io/name=$CHART,app.kubernetes.io/instance=$RELEASE \
+#             -o=jsonpath='{.items[0].metadata.name}')
 
-        kubectl exec -n $NAMESPACE -it $pod -- \
-            jupyter nbconvert --execute egeria-server-config.ipynb
+#         kubectl exec -n $NAMESPACE -it $pod -- \
+#             jupyter nbconvert --execute egeria-server-config.ipynb
 
-        kubectl exec -n $NAMESPACE -it $pod -- \
-            jupyter nbconvert --execute egeria-server-start.ipynb \
-            --ExecutePreprocessor.timeout=-1
-    }
+#         kubectl exec -n $NAMESPACE -it $pod -- \
+#             jupyter nbconvert --execute egeria-server-start.ipynb \
+#             --ExecutePreprocessor.timeout=-1
+#     }
 
 case "$1" in
     deploy)
         undeploy
         deploy
-        config
         ;;
     undeploy)
         undeploy
