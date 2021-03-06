05eeeb9f1c7d3c6f200efe9a132ccc4bc69f8dc4
rohithdv <rovallam@in.ibm.com>
2021-03-07 00:43:01 +0530

    updates for making json loading in kube-mgmt possible
    Signed-off-by: rohithdv <rovallam@in.ibm.com>

--- third_party/opa/delete_and_redeploy_opa.sh
+++ third_party/opa/delete_and_redeploy_opa.sh
@@ -1,18 +1,23 @@
-#!/bin/bash 
+#!/bin/bash
 
+kubectl delete -f policy-editor.yaml
+kubectl delete -f policy-editor-rolebinding.yaml
 kubectl delete -f opa.yaml
 kubectl delete configmap opa-json
 kubectl delete configmap opa-policy
 kubectl scale deployment opa-connector  --replicas=0
 kubectl scale deployment opa-connector  --replicas=1
 
-kubectl create -f opa.yaml 
+kubectl create -f policy-editor.yaml
+kubectl create -f policy-editor-rolebinding.yaml
+
+kubectl create -f opa.yaml
 cd jsonfiles
 
-kubectl kustomize ./ |kubectl label -f- --dry-run=client -o yaml --local openpolicyagent.org/data=opa > newjson.yaml 
+kubectl kustomize ./ |kubectl label -f- --dry-run=client -o yaml --local openpolicyagent.org/data=opa > newjson.yaml
 kubectl create -f newjson.yaml
 
 cd ../data-and-policies
 
-kubectl kustomize ./ |kubectl label -f- --dry-run=client -o yaml --local openpolicyagent.org/policy=rego > newpolicy.yaml 
+kubectl kustomize ./ |kubectl label -f- --dry-run=client -o yaml --local openpolicyagent.org/policy=rego > newpolicy.yaml
 kubectl create -f newpolicy.yaml
