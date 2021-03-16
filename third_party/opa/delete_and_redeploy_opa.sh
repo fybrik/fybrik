fc3516e43d70ef5904df71049251dc09af3c2628
rohithdv <rovallam@in.ibm.com>
2021-03-16 16:03:44 +0530

    updated dir structure to handle Roee's comments in https://github.com/IBM/the-mesh-for-data/discussions/420#discussioncomment-486627
    Signed-off-by: rohithdv <rovallam@in.ibm.com>

--- third_party/opa/delete_and_redeploy_opa.sh
+++ third_party/opa/delete_and_redeploy_opa.sh
@@ -3,21 +3,38 @@
 kubectl delete -f policy-editor.yaml
 kubectl delete -f policy-editor-rolebinding.yaml
 kubectl delete -f opa.yaml
-kubectl delete configmap opa-json
-kubectl delete configmap opa-policy
+kubectl delete configmap meshfordata-external-data
+kubectl delete configmap meshfordata-policy-lib
+kubectl delete configmap user1-policies
+kubectl delete configmap user2-policies
 kubectl scale deployment opa-connector  --replicas=0
 kubectl scale deployment opa-connector  --replicas=1
 
+cd meshfordata-external-data
+rm meshfordata-external-data.yaml
+cd ../meshfordata-policy-lib
+rm meshfordata-policy-lib.yaml
+cd ../user1-policies
+rm user1-policies.yaml
+cd ../user2-policies
+rm user2-policies.yaml
+
 kubectl create -f policy-editor.yaml
 kubectl create -f policy-editor-rolebinding.yaml
-
 kubectl create -f opa.yaml
-cd jsonfiles
 
-kubectl kustomize ./ |kubectl label -f- --dry-run=client -o yaml --local openpolicyagent.org/data=opa > newjson.yaml
-kubectl create -f newjson.yaml
+cd data-and-policies/meshfordata-external-data
+kubectl kustomize ./ |kubectl label -f- --dry-run=client -o yaml --local openpolicyagent.org/data=opa > meshfordata-external-data.yaml
+kubectl create -f meshfordata-external-data.yaml
 
-cd ../data-and-policies
+cd ../meshfordata-policy-lib
+kubectl kustomize ./ |kubectl label -f- --dry-run=client -o yaml --local openpolicyagent.org/policy=rego > meshfordata-policy-lib.yaml
+kubectl create -f meshfordata-policy-lib.yaml
 
-kubectl kustomize ./ |kubectl label -f- --dry-run=client -o yaml --local openpolicyagent.org/policy=rego > newpolicy.yaml
-kubectl create -f newpolicy.yaml
+cd ../user1-policies
+kubectl kustomize ./ |kubectl label -f- --dry-run=client -o yaml --local openpolicyagent.org/policy=rego > user1-policies.yaml
+kubectl create -f user1-policies.yaml
+
+cd ../user2-policies
+kubectl kustomize ./ |kubectl label -f- --dry-run=client -o yaml --local openpolicyagent.org/policy=rego > user2-policies.yaml
+kubectl create -f user2-policies.yaml
