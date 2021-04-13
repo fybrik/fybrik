a5adff2b87338437eee1d183b6c8402649683b44
eletonia <elentonia@gmail.com>
2021-04-13 12:07:02 -0400

    modifying script to group yaml files into folders
    Signed-off-by: eletonia <elentonia@gmail.com>

--- samples/operate-first/filterClusterScoped.py
+++ samples/operate-first/filterClusterScoped.py
@@ -1,48 +1,26 @@
 #!/usr/bin/python
 import yaml
+import os
+
+
+def splitToYamls(yamlFile):
+    with open(yamlFile, 'r') as file:
+        m4d_list = yaml.load_all(file, Loader=yaml.FullLoader)
+        for yml in m4d_list:
+            if yml:
+                filename = yml['metadata']['name']
+                outputFolder = yml['kind'].lower()+'s'
+                outputPath = os.path.join(outputFolder, filename + ".yaml")
+                if not os.path.exists(os.path.dirname(outputPath)):
+                    os.mkdir(os.path.dirname(outputPath))
+                f = open(outputPath, "w") 
+                documents = yaml.dump(yml, f)
+                f.close()          
 
 def main():
-    # Read in the output of helm template . for m4d resources
-    with open(r'm4d.yaml') as file:
-        m4d_files = []
-        for ln in file:
-            if ln.startswith("# Source:"):
-                filename = ln.split(': ')[1]
-                filename = filename.split('/')[-1].strip()
-                m4d_files.append(filename)
-    
-    # Filter out cluster-scoped resources and write to individual yaml files
-    with open(r'm4d.yaml') as file:
-        i = 0
-        m4d_list = yaml.load_all(file, Loader=yaml.FullLoader)
-        for yml in m4d_list:
-            if yml['kind'] == 'MutatingWebhookConfiguration' or yml['kind'] == 'ClusterRole' or yml['kind'] == 'ClusterRoleBinding' or yml['kind'] == 'Namespace':
-                outputFile = m4d_files[i]
-                f = open(outputFile, "w")
-                documents = yaml.dump(yml, f)
-                f.close()
-            i+=1
-
-    # Read in the output of helm template . for m4d-crd resources
-    with open(r'm4d-crd.yaml') as file:  
-        crd_files = []
-        for ln in file:
-            if ln.startswith("# Source:"):
-                filename = ln.split(': ')[1]
-                filename = filename.split('/')[-1].strip()
-                crd_files.append(filename)
-    
-     # Write CRDs to individual yaml files
-    with open(r'm4d-crd.yaml') as file:
-        i = 0
-        m4d_list = yaml.load_all(file, Loader=yaml.FullLoader)
-        for yml in m4d_list:
-            outputFile = crd_files[i]
-            f = open(outputFile, "w")
-            documents = yaml.dump(yml, f)
-            f.close()
-            i+=1
-
+    splitToYamls('m4d.yaml')
+    splitToYamls('m4d-crd.yaml')
 
 if __name__ == "__main__":
     main()
+    
