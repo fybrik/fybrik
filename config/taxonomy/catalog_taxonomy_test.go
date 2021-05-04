6a2323256b440bc21894c9c8d022c0a446f8bb21
Ritwik <ritwik@datameshproj.sl.cloud9.ibm.com>
2021-05-04 09:18:57 +0000

    OPA Taxonomy Code Updated
    Signed-off-by: Ritwik <ritwik@datameshproj.sl.cloud9.ibm.com>

--- config/taxonomy/catalog_taxonomy_test.go
+++ config/taxonomy/catalog_taxonomy_test.go
@@ -25,25 +25,31 @@ var (
 	resourceGoodNameOnly = "{\"resource\": {\"name\":\"file1\"}}"
 
 	// {"resource":{"name":"file1", "tags":[{"geography":{"name":"Turkey"}}]}}
-	resourceGoodNameGeo = "{\"resource\": {\"name\":\"file1\", \"tags\":[{\"geography\":{\"name\":\"Turkey\"}}]}}"
+	resourceGoodNameGeo = "{\"resource\": {\"name\":\"file1\", \"tags\":{\"residency\":\"Turkey\", \"asset\":\"PII\"}}}"
 
 	// {"resource":{"name":"file1", "tags":[{"geography":{"name":"Turkey"}}], "columns":[{"name":"col1"}, {"name":"col2"}]}}
-	resourceGoodCols = "{\"resource\":{\"name\":\"file1\", \"tags\":[{\"geography\":{\"name\":\"Turkey\"}}], \"columns\":[{\"name\":\"col1\"}, {\"name\":\"col2\"}]}}"
+	resourceGoodCols = "{\"resource\":{\"name\":\"file1\", \"tags\":{\"residency\":\"Turkey\", \"asset\":\"PII\"}, \"columns\":[{\"name\":\"col1\"}, {\"name\":\"col2\"}]}}"
 
 	// {"resource":{"name":"file1", "tags":[{"geography":{"name":"Turkey"}}], "columns":[{"name":"col1", "tags":[{"geography_name":"Turkey"}]}, {"name":"col2"}]}}
-	resourceGoodColsTags = "{\"resource\":{\"name\":\"file1\", \"tags\":[{\"geography\":{\"name\":\"Turkey\"}}], \"columns\":[{\"name\":\"col1\", \"tags\":[{\"geography_name\":\"Turkey\"}]}, {\"name\":\"col2\"}]}}"
+	resourceGoodColsTags = "{\"resource\":{\"name\":\"file1\", \"tags\":{\"residency\":\"Turkey\", \"asset\":\"PII\"}, \"columns\":[{\"name\":\"col1\", \"tags\":{\"residency\":\"Turkey\"}}, {\"name\":\"col2\"}]}}"
 
 	// {"resource":{"tags":[{"geography":{"name":"Turkey"}}]}}
-	resourceBadNoName = "{\"resource\": {\"tags\":[{\"geography\":{\"name\":\"Turkey\"}}]}}"
+	resourceBadNoName = "{\"resource\": {\"tags\":{\"residency\":\"Turkey\", \"asset\":\"PII\"}}}"
 
 	// {"resource":{"name":"file1", "tags":[{"geography":{"name":"xxx"}}]}}
-	resourceBadInvalidGeo = "{\"resource\":{\"name\":\"file1\", \"tags\":[{\"geography\":{\"name\":\"xxx\"}}]}}"
+	//resourceBadInvalidGeo = "{\"resource\":{\"name\":\"file1\", \"tags\":{\"residency\": 12, \"asset\":\"PII\"}}}"
 
 	// {"resource":{"name":"file1", "tags":[{"geography":{"name":"Turkey"}}], "columns":[{"name":"col1", "tags":[{"geography_name":"xxx"}]}, {"name":"col2"}]}}
-	resourceBadInvalidTagVal = "{\"resource\":{\"name\":\"file1\", \"tags\":[{\"geography\":{\"name\":\"Turkey\"}}], \"columns\":[{\"name\":\"col1\", \"tags\":[{\"geography_name\":\"xxx\"}]}, {\"name\":\"col2\"}]}}"
+	//resourceBadInvalidTagVal = "{\"resource\":{\"name\":\"file1\", \"tags\":[\"residency\":\"Turkey\", \"asset\":\"PII\"], \"columns\":[{\"name\":\"col1\", \"tags\":[\"residency\":\"Turkey\"]}, {\"name\":\"col2\"}]}}"
 
 	// {"resource":{"name":"file1", "tags":[{"geography":{"name":"Turkey"}}], "columns":[{"name":"col1", "tags":[{"badkey":"Turkey"}]}, {"name":"col2"}]}}
-	resourceGoodInvalidTagKey = "{\"resource\":{\"name\":\"file1\", \"tags\":[{\"geography\":{\"name\":\"Turkey\"}}], \"columns\":[{\"name\":\"col1\", \"tags\":[{\"badkey\":\"Turkey\"}]}, {\"name\":\"col2\"}]}}"
+	//resourceGoodInvalidTagKey = "{\"resource\":{\"name\":\"file1\", \"tags\":[residency:\"Turkey\", \"asset\":\"PII\"], \"columns\":[{\"name\":\"col1\", \"tags\":{residency:\"Turkey\"}}, {\"name\":\"col2\"}]}}"
+	
+	//"{\"resource\":{\"name\":\"file1\", \"tags\":[{\"geography\":{\"name\":\"Turkey\"}}], \"columns\":[{\"name\":\"col1\", \"tags\":[{\"geography_name\":\"Turkey\"}]}, {\"name\":\"col2\"}]}}"
+	resourceGoodmetadata = "{\"resource\":{\"name\":\"file1\", \"metadata\":{\"creationTime\":\"1620022174006\", \"createdBy\":\"1000\", \"updationTime\":\"1620022175000\", \"updatedBy\":\"1002\", \"totalRating\": \"4.7\"}}}"
+
+	// {\"resource\":{\"name\":\"file1\", \"tags\":[{\"geography\":{\"name\":\"Turkey\"}}], \"columns\":[{\"name\":\"col1\", \"tags\":[{\"badkey\":\"Turkey\"}]}, {\"name\":\"col2\"}]}}
+	resourceBadmetadata = "{\"resource\":{\"name\":\"file1\", \"metadata\":{\"creationTime\": 1620022174006, \"createdBy\":\"1000\", \"updationTime\":\"1620022175000\", \"updatedBy\":\"1002\", \"totalRating\": \"4.7\"}}}"
 )
 
 func TestCatalogTaxonomy(t *testing.T) {
@@ -63,7 +69,11 @@ func TestCatalogTaxonomy(t *testing.T) {
 	ValidateTaxonomy(t, catalogTaxStructsName, resourceGoodColsTags, "resourceGoodColsTags", true)
 
 	ValidateTaxonomy(t, catalogTaxStructsName, resourceBadNoName, "resourceBadNoName", false)
-	ValidateTaxonomy(t, catalogTaxStructsName, resourceBadInvalidGeo, "resourceBadInvalidGeo", false)
-	ValidateTaxonomy(t, catalogTaxStructsName, resourceBadInvalidTagVal, "resourceBadInvalidTagVal", false)
-	ValidateTaxonomy(t, catalogTaxStructsName, resourceGoodInvalidTagKey, "resourceBadInvalidTagKey", true)
+	//ValidateTaxonomy(t, catalogTaxStructsName, resourceBadInvalidGeo, "resourceBadInvalidGeo", false)
+	//ValidateTaxonomy(t, catalogTaxStructsName, resourceBadInvalidTagVal, "resourceBadInvalidTagVal", false)
+	//ValidateTaxonomy(t, catalogTaxStructsName, resourceGoodInvalidTagKey, "resourceBadInvalidTagKey", true)
+
+	ValidateTaxonomy(t, catalogTaxStructsName, resourceGoodmetadata, "resourceGoodmetadata", true)
+	ValidateTaxonomy(t, catalogTaxStructsName, resourceBadmetadata, "resourceBadmetadata", false)
+
 }
--- pkg/taxonomy/opa_taxonomy_test.go
+++ pkg/taxonomy/opa_taxonomy_test.go
@@ -11,16 +11,11 @@ import (
 var (
 	OPATaxValsName    = "../../config/taxonomy/opa.structs.schema.json"
 
-governanceRequestGood = "{\"opaGovernanceRequest\": {\"request_context\":{\"intent\":\"Marketing\", \"role\":\"Data Scientist\"},\"action\":{\"action_type\":\"read\", \"processingLocation\":\"Turkey\"}, \"resource\":{\"name\":\"file1\"}}}"
-governanceRequestBadNoResource = "{\"opaGovernanceRequest\": {\"request_context\":{\"intent\":\"Marketing\", \"role\":\"Data Scientist\"},\"action\":{\"action_type\":\"read\", \"processingLocation\":\"Turkey\"}}}"
-governanceResponseGood = "{\"opaGovernanceResponse\": {\"resource\":{\"name\":\"file1\"}, \"governance_decision\":\"allow\"}}"
-governanceResponseBadNoDecision = "{\"opaGovernanceResponse\": {\"resource\":{\"name\":\"file1\"}}}"
-
+governanceRequestGood = "{\"request_context\":{\"intent\":\"Marketing\", \"role\":\"Data Scientist\"},\"action\":{\"action_type\":\"read\", \"processingLocation\":\"Turkey\"}, \"resource\":{\"name\":\"file1\"}}"
+governanceRequestBadNoResource = "{\"request_context\":{\"intent\":\"Marketing\", \"role\":\"Data Scientist\"},\"action\":{\"action_type\":\"read\", \"processingLocation\":\"Turkey\"}}"
 )
 
 func TestOPAInputTaxonomy(t *testing.T) {
 	tax.ValidateTaxonomy(t, OPATaxValsName, governanceRequestGood, "governanceRequestGood", true)
 	tax.ValidateTaxonomy(t, OPATaxValsName, governanceRequestBadNoResource, "governanceRequestBadNoResource", false)
-	tax.ValidateTaxonomy(t, OPATaxValsName, governanceResponseGood, "governanceResponseGood", true)
-	tax.ValidateTaxonomy(t, OPATaxValsName, governanceResponseBadNoDecision, "governanceResponseBadNoDecision", false)
 }
