d3b18908cce8ec57e33136fca6bdc1bfb95fcb10
eletonia <elentonia@gmail.com>
2021-05-07 16:40:15 -0400

    changing directory structure, renaming yaml files, and modifying kustomization files

--- samples/operate-first/filterClusterScoped.py
+++ samples/operate-first/filterClusterScoped.py
@@ -8,27 +8,29 @@ def splitToYamls(yamlFile):
         m4d_list = yaml.load_all(file, Loader=yaml.FullLoader)
         for yml in m4d_list:
             if yml:
-                filename = yml['metadata']['name']
+                filename = yml['kind'].lower()
+                resourceName = yml['metadata']['name']
                 outputFolder = yml['kind'].lower()+'s'
-                outputPath = os.path.join(outputFolder, 'm4d-system', filename + ".yaml")
+                outputPath = os.path.join(outputFolder, resourceName, filename + ".yaml")
                 os.makedirs(os.path.dirname(outputPath), exist_ok = True)
                 with open(outputPath,'w') as f:
                     yaml.dump(yml, f)    
 
 def createKustomizations():
     dirs = [name for name in os.listdir(".") if os.path.isdir(name)]
-    for subdir in dirs:
-        outputPath = os.path.join(subdir,'m4d-system','kustomization.yaml')
-        kustomizationString = "---\napiVersion: kustomize.config.k8s.io/v1beta1\nkind: Kustomization" + "\n\nresources:\n"
-        subdirPath = os.path.join(".", subdir,'m4d-system')
-        for root, subdirs, files in os.walk(subdirPath):
-            for file in files:
-                if file != 'kustomization.yaml':
-                    kustomizationString += "  - " + file +'\n'
-        with open (outputPath, 'w') as f:
-            f.write(kustomizationString)
-    
-      
+    subdirPaths = []
+    for folder in dirs:
+        subdirPaths.append(os.path.join(".", folder))
+    for path in subdirPaths:
+        resources = os.listdir(path)  
+        for resource in resources:
+            resourcePath = os.path.join(path, resource)
+            kustomizationString = "apiVersion: kustomize.config.k8s.io/v1beta1\nkind: Kustomization" + "\nresources:\n"
+            yamlFile = path[2:-1] + '.yaml'
+            kustomizationString += "  - ./" + yamlFile +'\n'
+            outputPath = os.path.join(resourcePath,'kustomization.yaml')
+            with open (outputPath, 'w') as f:
+                f.write(kustomizationString)
 
 def main():
     splitToYamls('m4d.yaml')
