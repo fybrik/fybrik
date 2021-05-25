9d7f048728b03fe90cd0db3fe28a555bf0438257
Ritwik <ritwik@datameshproj.sl.cloud9.ibm.com>
2021-05-25 09:16:29 +0000

    corrected linting issues
    Signed-off-by: Ritwik <ritwik@datameshproj.sl.cloud9.ibm.com>

--- config/taxonomy/module_taxonomy_test.go
+++ config/taxonomy/module_taxonomy_test.go
@@ -8,40 +8,40 @@ import (
 )
 
 var (
-	ModuleTaxStructsName = "module.structs.schema.json"
-	ModuleTaxValsName    = "module.values.schema.json"
+	// ModuleTaxStructsName = "module.structs.schema.json"
+	ModuleTaxValsName = "module.values.schema.json"
 
 	moduleTypeGood = "{\"module_type\":\"read\"}"
 	moduleTypeBad  = "{\"module_type\":\"xxx\"}"
 
-	//governanceDecisionNameGood = "{\"action_name\":\"RedactColumn\"}"
-	//governanceDecisionNameBad  = "{\"action_name\":\"xxx\"}"
+	// governanceDecisionNameGood = "{\"action_name\":\"RedactColumn\"}"
+	// governanceDecisionNameBad  = "{\"action_name\":\"xxx\"}"
 
 	protocolGood = "{\"protocol\":\"m4d-arrow-flight\"}"
 	protocolBad  = "{\"protocol\":\"xxx\"}"
 
 	// {"transformation": {"name":"mask"}}
-	//governanceDecisionBaseGood = "{\"action\": {\"name\":\"RedactColumn\"}}"
+	// governanceDecisionBaseGood = "{\"action\": {\"name\":\"RedactColumn\"}}"
 
 	// {"transformation": {"name":"xxx"}}
-	//governanceDecisionBaseBad = "{\"action\": {\"name\":\"xxx\"}}"
+	// governanceDecisionBaseBad = "{\"action\": {\"name\":\"xxx\"}}"
 
 	// {"transformation": {"name":"mask", "params":["param1", "params2"]}}
-	//governanceDecisionGoodParams = "{\"action\": {\"name\":\"RemoveColumn\", \"params\":[\"Column 1\", \"params2\"]}}"
+	// governanceDecisionGoodParams = "{\"action\": {\"name\":\"RemoveColumn\", \"params\":[\"Column 1\", \"params2\"]}}"
 
 	// {"transformation": {"params":["param1", "params2"]}}
-	//governanceDecisionBadNoName = "{\"action\": {\"params\":[\"Column 1\", \"params2\"]}}"
+	// governanceDecisionBadNoName = "{\"action\": {\"params\":[\"Column 1\", \"params2\"]}}"
 )
 
 func TestModuleTaxonomy(t *testing.T) {
 	ValidateTaxonomy(t, ModuleTaxValsName, moduleTypeGood, "moduleTypeGood", true)
 	ValidateTaxonomy(t, ModuleTaxValsName, moduleTypeBad, "moduleTypeBad", false)
-	//ValidateTaxonomy(t, ModuleTaxValsName, governanceDecisionNameGood, "governanceDecisionNameGood", true)
-	//ValidateTaxonomy(t, ModuleTaxValsName, governanceDecisionNameBad, "governanceDecisionNameBad", false)
+	// ValidateTaxonomy(t, ModuleTaxValsName, governanceDecisionNameGood, "governanceDecisionNameGood", true)
+	// ValidateTaxonomy(t, ModuleTaxValsName, governanceDecisionNameBad, "governanceDecisionNameBad", false)
 	ValidateTaxonomy(t, ModuleTaxValsName, protocolGood, "protocolGood", true)
 	ValidateTaxonomy(t, ModuleTaxValsName, protocolBad, "protocolBad", false)
-	//ValidateTaxonomy(t, ModuleTaxStructsName, governanceDecisionBaseGood, "governanceDecisionBaseGood", true)
-	//ValidateTaxonomy(t, ModuleTaxStructsName, governanceDecisionBaseBad, "governanceDecisionBaseBad", false)
-	//ValidateTaxonomy(t, ModuleTaxStructsName, governanceDecisionGoodParams, "governanceDecisionGoodParams", true)
-	//ValidateTaxonomy(t, ModuleTaxStructsName, governanceDecisionBadNoName, "governanceDecisionBadNoName", false)
+	// ValidateTaxonomy(t, ModuleTaxStructsName, governanceDecisionBaseGood, "governanceDecisionBaseGood", true)
+	// ValidateTaxonomy(t, ModuleTaxStructsName, governanceDecisionBaseBad, "governanceDecisionBaseBad", false)
+	// ValidateTaxonomy(t, ModuleTaxStructsName, governanceDecisionGoodParams, "governanceDecisionGoodParams", true)
+	// ValidateTaxonomy(t, ModuleTaxStructsName, governanceDecisionBadNoName, "governanceDecisionBadNoName", false)
 }
