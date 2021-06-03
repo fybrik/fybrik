664cdbe977f082315194b1a6472661a7036aa1ed
roee88 <roee88@gmail.com>
2021-06-03 16:36:19 +0300

    Fix: linter issues
    Signed-off-by: roee88 <roee88@gmail.com>

--- pkg/serde/arbitrary.go
+++ pkg/serde/arbitrary.go
@@ -24,13 +24,13 @@ func (in *Arbitrary) DeepCopyInto(out *Arbitrary) {
 	*out = *copy
 }
 
-func (p *Arbitrary) UnmarshalJSON(data []byte) error {
-	if err := json.Unmarshal(data, &p.Data); err != nil {
+func (in *Arbitrary) UnmarshalJSON(data []byte) error {
+	if err := json.Unmarshal(data, &in.Data); err != nil {
 		return err
 	}
 	return nil
 }
 
-func (p *Arbitrary) MarshalJSON() ([]byte, error) {
-	return json.Marshal(p.Data)
+func (in *Arbitrary) MarshalJSON() ([]byte, error) {
+	return json.Marshal(in.Data)
 }
