d0788902a45200ce27814dc00319d61ef462d80a
Revital Sur <eres@il.ibm.com>
2021-06-08 06:45:34 +0300

    Create new helm chart for Vault deployment.
    Fixes part of #632
    
    Signed-off-by: Revital Sur <eres@il.ibm.com>

--- hack/release.sh
+++ hack/release.sh
@@ -9,3 +9,5 @@ ${TOOLBIN}/yq eval --inplace ".version = \"$RELEASE\"" ./charts/m4d/Chart.yaml
 ${TOOLBIN}/yq eval --inplace ".appVersion = \"$RELEASE\"" ./charts/m4d/Chart.yaml
 ${TOOLBIN}/yq eval --inplace ".version = \"$RELEASE\"" ./charts/m4d-crd/Chart.yaml
 ${TOOLBIN}/yq eval --inplace ".appVersion = \"$RELEASE\"" ./charts/m4d-crd/Chart.yaml
+${TOOLBIN}/yq eval --inplace ".version = \"$RELEASE\"" ./charts/vault/Chart.yaml
+${TOOLBIN}/yq eval --inplace ".appVersion = \"$RELEASE\"" ./charts/vault/Chart.yaml
