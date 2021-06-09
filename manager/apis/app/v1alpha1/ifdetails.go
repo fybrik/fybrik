ba97cc49f809da35e81ac53bbe62720bfafc4b3a
Shlomit Koyfman <shlomitk@il.ibm.com>
2021-06-09 09:29:42 +0300

    getting rid of hard-coded types for protocol and data format
    Signed-off-by: Shlomit Koyfman <shlomitk@il.ibm.com>

--- manager/apis/app/v1alpha1/ifdetails.go
+++ manager/apis/app/v1alpha1/ifdetails.go
@@ -3,38 +3,24 @@
 
 package v1alpha1
 
-// IFProtocol defines interface protocol for data transactions
-type IFProtocol string
-
-// DataFormatType defines data format type
-type DataFormatType string
-
-// DataFormatType valid values
+// Values used in tests and mockup servers
 const (
-	Parquet DataFormatType = "parquet"
-	Table   DataFormatType = "table" // remove?
-	CSV     DataFormatType = "csv"
-	JSON    DataFormatType = "json"
-	AVRO    DataFormatType = "avro"
-	ORC     DataFormatType = "orc"
-	Binary  DataFormatType = "binary"
-	Arrow   DataFormatType = "arrow"
-)
-
-// IFProtocol valid values
-const (
-	S3          IFProtocol = "s3"
-	Kafka       IFProtocol = "kafka"
-	JdbcDb2     IFProtocol = "jdbc-db2"
-	ArrowFlight IFProtocol = "m4d-arrow-flight"
+	S3          string = "s3"
+	Kafka       string = "kafka"
+	JdbcDb2     string = "jdbc-db2"
+	ArrowFlight string = "m4d-arrow-flight"
+	Arrow       string = "arrow"
+	Parquet     string = "parquet"
+	Table       string = "table"
 )
 
 // InterfaceDetails indicate how the application or module receive or write the data
 type InterfaceDetails struct {
-
+	// Protocol defines the interface protocol used for data transactions
 	// +required
-	Protocol IFProtocol `json:"protocol"`
+	Protocol string `json:"protocol"`
 
+	// DataFormat defines the data format type
 	// +optional
-	DataFormat DataFormatType `json:"dataformat,omitempty"` // To be removed in future
+	DataFormat string `json:"dataformat,omitempty"` // To be removed in future
 }
