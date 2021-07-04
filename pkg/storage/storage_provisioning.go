b8f70197e1a229543bb404b50e525c9c142367d0
SHLOMITK@il.ibm.com <shlomitk@il.ibm.com>
2021-07-04 17:21:36 +0300

    unnecessary definitions
    Signed-off-by: SHLOMITK@il.ibm.com <shlomitk@il.ibm.com>

--- pkg/storage/storage_provisioning.go
+++ pkg/storage/storage_provisioning.go
@@ -19,7 +19,6 @@ import (
 	"fmt"
 
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/scheme"
 
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -29,11 +28,6 @@ import (
 var (
 	// GroupVersion is group version used to register these objects
 	GroupVersion = schema.GroupVersion{Group: "com.ie.ibm.hpsys", Version: "v1alpha1"}
-	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
-	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
-
-	// AddToScheme adds the types in this group-version to the given scheme.
-	AddToScheme = SchemeBuilder.AddToScheme
 )
 
 // ProvisionedBucket holds information about the bucket to be provisioned.
