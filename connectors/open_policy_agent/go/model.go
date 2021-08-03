ae84d2c3e0f24ecdeae7ac6b9d674cfdef0f2d3f
Ritwik <ritwik@datameshproj.sl.cloud9.ibm.com>
2021-08-03 12:05:42 +0000

    OpenAPI generated server code
    Signed-off-by: Ritwik <ritwik@datameshproj.sl.cloud9.ibm.com>

---
+++ connectors/open_policy_agent/go/model.go
@@ -0,0 +1,6 @@
+package openapiserver
+import (
+	"fybrik.io/fybrik/pkg/taxonomy/model/base"
+)
+type PolicyManagerRequest = base.PolicyManagerRequest
+type PolicyManagerResponse = base.PolicyManagerResponse
