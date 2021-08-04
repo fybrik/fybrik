9ae3fc301754abe49ae1aa5f37d9470ba110f2e2
Ritwik <ritwik@datameshproj.sl.cloud9.ibm.com>
2021-08-04 06:08:47 +0000

    resolving linting issue in generated code
    Signed-off-by: Ritwik <ritwik@datameshproj.sl.cloud9.ibm.com>

--- connectors/open_policy_agent/main.go
+++ connectors/open_policy_agent/main.go
@@ -11,15 +11,6 @@ package main
 
 import (
 	"log"
-	
-	// WARNING!
-	// Change this to a fully-qualified import path
-	// once you place this file into your project.
-	// For example,
-	//
-	//sw "github.com/GIT_USER_ID/GIT_REPO_ID/go"
-	//
-	
 	sw "fybrik.io/fybrik/connectors/open_policy_agent/go"
 )
 
