c338a4f97bfd8ed071de44d369e429976dbfcdb4
Rohith D Vallam <rovallam@in.ibm.com>
2021-08-09 17:19:19 +0000

    updated opa connector mock with rest api based implementation
    Signed-off-by: Rohith D Vallam <rovallam@in.ibm.com>

--- test/services/policymanager/serverpolicymanager.go
+++ test/services/policymanager/serverpolicymanager.go
@@ -3,24 +3,71 @@
 
 package main
 
-// const (
-// 	PORT = 50082
-// )
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	mockup "fybrik.io/fybrik/manager/controllers/mockup"
+	openapiclientmodels "fybrik.io/fybrik/pkg/taxonomy/model/base"
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	PORT = 50082
+)
+
+// func main() {
+// 	address := utils.ListeningAddress(PORT)
+// 	log.Printf("starting mock policy manager server on address %s", address)
+
+// 	listener, err := net.Listen("tcp", address)
+// 	if err != nil {
+// 		log.Fatalf("listening error: %v", err)
+// 	}
+
+// 	server := grpc.NewServer()
+// 	service := &mockup.MockPolicyManager{}
+
+// 	pb.RegisterPolicyManagerServiceServer(server, service)
+// 	if err := server.Serve(listener); err != nil {
+// 		log.Fatalf("cannot serve mock policy manager: %v", err)
+// 	}
+
+// }
+
+var router *gin.Engine
+
+func constructPolicyManagerRequest(inputString string) *openapiclientmodels.PolicyManagerRequest {
+	fmt.Println("inconstructPolicymanagerRequest")
+	fmt.Println("inputString")
+	fmt.Println(inputString)
+	var input openapiclientmodels.PolicyManagerRequest
+	json.Unmarshal([]byte(inputString), &input)
+	fmt.Println("input:", input)
+	return &input
+}
 
 func main() {
-	// address := utils.ListeningAddress(PORT)
-	// log.Printf("starting mock policy manager server on address %s", address)
+	router = gin.Default()
 
-	// listener, err := net.Listen("tcp", address)
-	// if err != nil {
-	// 	log.Fatalf("listening error: %v", err)
-	// }
+	router.GET("/getPoliciesDecisions", func(c *gin.Context) {
+		input := c.Query("input")
+		creds := c.Query("creds")
+		policyManagerReq := constructPolicyManagerRequest(input)
+		policyManager := &mockup.MockPolicyManager{}
+		policyManagerResp, err := policyManager.GetPoliciesDecisions(policyManagerReq, creds)
+		if err != nil {
+			c.String(http.StatusInternalServerError, "Error in GetPoliciesDecisions!")
+			return
+		}
+		c.JSON(http.StatusOK, policyManagerResp)
+	})
 
-	// server := grpc.NewServer()
-	// service := &mockup.MockPolicyManager{}
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello World!")
+	})
 
-	// pb.RegisterPolicyManagerServiceServer(server, service)
-	// if err := server.Serve(listener); err != nil {
-	// 	log.Fatalf("cannot serve mock policy manager: %v", err)
-	// }
+	router.Run(":" + strconv.Itoa(PORT))
 }
