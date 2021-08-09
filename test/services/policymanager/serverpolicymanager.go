fc9640ad4189aaaedd7e7d76bd5e266eea54aace
Rohith D Vallam <rovallam@in.ibm.com>
2021-08-09 17:38:07 +0000

    removed commented code
    Signed-off-by: Rohith D Vallam <rovallam@in.ibm.com>

--- test/services/policymanager/serverpolicymanager.go
+++ test/services/policymanager/serverpolicymanager.go
@@ -19,25 +19,6 @@ const (
 	PORT = 50082
 )
 
-// func main() {
-// 	address := utils.ListeningAddress(PORT)
-// 	log.Printf("starting mock policy manager server on address %s", address)
-
-// 	listener, err := net.Listen("tcp", address)
-// 	if err != nil {
-// 		log.Fatalf("listening error: %v", err)
-// 	}
-
-// 	server := grpc.NewServer()
-// 	service := &mockup.MockPolicyManager{}
-
-// 	pb.RegisterPolicyManagerServiceServer(server, service)
-// 	if err := server.Serve(listener); err != nil {
-// 		log.Fatalf("cannot serve mock policy manager: %v", err)
-// 	}
-
-// }
-
 var router *gin.Engine
 
 func constructPolicyManagerRequest(inputString string) *openapiclientmodels.PolicyManagerRequest {
