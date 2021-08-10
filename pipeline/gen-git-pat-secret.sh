1c533392f1960889e877ac5e67e293854759f579
Nicholas Goracke <ngoracke@us.ibm.com>
2021-08-10 09:39:08 -0500

    remove references to ibm url
    Signed-off-by: Nicholas Goracke <ngoracke@us.ibm.com>

--- pipeline/gen-git-pat-secret.sh
+++ pipeline/gen-git-pat-secret.sh
@@ -2,7 +2,7 @@
 
 github_user=${1:?Need to specify the github (Enterprise) username}
 pat=${2:?Need to specify the personal access token}
-github_host=${3:-github.ibm.com}
+github_host=${3:-github.com}
 
 oc delete secret git-pat
 oc create secret generic git-pat --from-literal=username=${github_user} --from-literal=password=${pat} --type=kubernetes.io/basic-auth
--- pipeline/gen-git-ssh-secret.sh
+++ pipeline/gen-git-ssh-secret.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ssh_key=${1:-$HOME/.ssh/id_rsa}
-github_host=${2:-github.ibm.com}
+github_host=${2:-github.com}
 
 oc delete secret git-ssh-key
 oc create secret generic git-ssh-key --from-file=ssh-privatekey=${ssh_key} --type=kubernetes.io/ssh-auth
