3837dd72d358e489167f523e41c7d634699b38fa
Rohith D Vallam <rohithdv@gmail.com>
2021-09-13 21:27:16 +0530

    Add new openpolicyagent connector (V2) (#789)
    Signed-off-by: Rohith D Vallam <rovallam@in.ibm.com>

--- connectors/open_policy_agent/go/api_default.go
+++ connectors/open_policy_agent/go/api_default.go
@@ -13,18 +13,52 @@
 package openapiserver
 
 import (
-	"io/ioutil"
 	"log"
 	"net/http"
 
+	"emperror.dev/errors"
+	opabl "fybrik.io/fybrik/connectors/open_policy_agent/lib"
+	openapiclientmodels "fybrik.io/fybrik/pkg/taxonomy/model/base"
+	"github.com/gdexlab/go-render/render"
 	"github.com/gin-gonic/gin"
 )
 
-// GetPoliciesDecisions - getPoliciesDecisions
-func GetPoliciesDecisions(c *gin.Context) {
+type ConnectorController struct {
+	OpaServerURL  string
+	OpaReader     *opabl.OpaReader
+	CatalogReader *opabl.CatalogReader
+}
+
+func (e *ConnectorController) contactOPA(input openapiclientmodels.PolicyManagerRequest, creds string) (openapiclientmodels.PolicyManagerResponse, error) {
+	policyToBeEvaluated := "dataapi/authz/verdict"
+	eval, err := e.OpaReader.GetOPADecisions(&input, creds, e.CatalogReader, policyToBeEvaluated)
+	if err != nil {
+		return openapiclientmodels.PolicyManagerResponse{}, errors.Wrap(err, "GetOPADecisions error in contactOPA")
+	}
+
+	output := render.AsCode(eval)
+	log.Println("received PolicyManagerResponse from GetOPADecisions:", output)
+
+	return eval, nil
+}
+
+func (e *ConnectorController) GetPoliciesDecisions(c *gin.Context) {
 	log.Println("in GetPoliciesDecisions of V2 OPA Connector!")
-	data, _ := ioutil.ReadAll(c.Request.Body)
-	log.Printf("ctx.Request.body: %v", string(data))
-	log.Println("creds value is", c.Request.Header["X-Request-Cred"][0])
-	c.JSON(http.StatusOK, gin.H{})
+	input := new(openapiclientmodels.PolicyManagerRequest)
+	if err := c.ShouldBindJSON(input); err != nil {
+		log.Println("Error during ShouldBindJSON:", err.Error())
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	output := render.AsCode(input)
+	log.Println("bound PolicyManagerRequest:", output)
+
+	resp, err := e.contactOPA(*input, c.Request.Header["X-Request-Cred"][0])
+	if err != nil {
+		log.Println("Error during contactOPA: ", err)
+		c.JSON(http.StatusInternalServerError, gin.H{})
+		return
+	}
+
+	c.JSON(http.StatusOK, resp)
 }
