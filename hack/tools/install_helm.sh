8d5ea1614783c41f8250e0a028d3972984c0527f
Florian Froese <ffr@zurich.ibm.com>
2021-09-20 15:46:46 +0200

    Fix helm version to 3.6.3
    Signed-off-by: Florian Froese <ffr@zurich.ibm.com>

--- hack/tools/install_helm.sh
+++ hack/tools/install_helm.sh
@@ -10,8 +10,10 @@ header_text "Checking for bin/helm"
 
 header_text "Installing bin/helm"
 
+# Helm is currently fixed to 3.6.3 until the deployment process is migrated to 3.7.0 (https://github.com/helm/helm/releases/tag/v3.7.0)
+# Some used experimental OCI features have been removed.
 mkdir -p ./bin
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod 700 get_helm.sh
-HELM_INSTALL_DIR=bin ./get_helm.sh --no-sudo
+HELM_INSTALL_DIR=bin DESIRED_VERSION=v3.6.3 ./get_helm.sh --no-sudo
 rm -rf get_helm.sh
