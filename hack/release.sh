274ea5da05127ddab7026f1ce758c8f93d8d427e
Revital Sur <eres@il.ibm.com>
2022-02-07 12:33:47 +0200

    Add version field to asset-crd dependency in fybrik-crd chart (#1265)
    Signed-off-by: Revital Sur <eres@il.ibm.com>

--- hack/release.sh
+++ hack/release.sh
@@ -9,6 +9,7 @@ ${TOOLBIN}/yq eval --inplace ".version = \"$RELEASE\"" ./charts/fybrik/Chart.yam
 ${TOOLBIN}/yq eval --inplace ".appVersion = \"$RELEASE\"" ./charts/fybrik/Chart.yaml
 ${TOOLBIN}/yq eval --inplace ".version = \"$RELEASE\"" ./charts/fybrik-crd/Chart.yaml
 ${TOOLBIN}/yq eval --inplace ".appVersion = \"$RELEASE\"" ./charts/fybrik-crd/Chart.yaml
+${TOOLBIN}/yq eval --inplace ".dependencies[0].version = \"$RELEASE\"" ./charts/fybrik-crd/Chart.yaml
 ${TOOLBIN}/yq eval --inplace ".version = \"$RELEASE\"" ./charts/fybrik-crd/charts/asset-crd/Chart.yaml
 ${TOOLBIN}/yq eval --inplace ".appVersion = \"$RELEASE\"" ./charts/fybrik-crd/charts/asset-crd/Chart.yaml
 ${TOOLBIN}/yq eval --inplace ".version = \"$RELEASE\"" ./charts/vault/Chart.yaml
