b70e4b714cab5313974e2a9a644cfc5aa0c795fa
Doron Chen <cdoron@il.ibm.com>
2022-02-23 19:31:05 +0200

    add python code
    Signed-off-by: Doron Chen <cdoron@il.ibm.com>

---
+++ pkg/logging/python_logging/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+import os
+
+setup(name='fybrik_python_logging',
+      version=os.environ.get('RELEASE', '0.1'),
+      description='Python Logging Package for Fybrik Components',
+      license='Apache License, Version 2.0',
+      packages=['.'],
+      install_requires=[
+          'JSON-log-formatter==0.5.0',
+      ],
+)
