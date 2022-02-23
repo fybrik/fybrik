5e2c36ec1dfb978bbd2bfcbbfacc658a1c84c038
Doron Chen <cdoron@il.ibm.com>
2022-02-23 19:38:08 +0200

    try
    Signed-off-by: Doron Chen <cdoron@il.ibm.com>

--- pkg/logging/python_logging/setup.py
+++ pkg/logging/python_logging/setup.py
@@ -2,7 +2,8 @@ from setuptools import setup
 import os
 
 setup(name='fybrik_python_logging',
-      version=os.environ.get('RELEASE', '0.1'),
+      #version=os.environ.get('RELEASE', '0.1'),
+      version='0.0.1',
       description='Python Logging Package for Fybrik Components',
       license='Apache License, Version 2.0',
       packages=['.'],
