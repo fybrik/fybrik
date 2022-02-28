911f72aa35193e8835d559f903dfda95882c1c75
Doron Chen <cdoron@il.ibm.com>
2022-02-28 16:41:25 +0200

    changes following review comments
    Signed-off-by: Doron Chen <cdoron@il.ibm.com>

--- python/logging/setup.py
+++ python/logging/setup.py
@@ -1,13 +1,12 @@
 from setuptools import setup
-import os
 
 setup(name='fybrik_python_logging',
-      version=os.environ.get('RELEASE', '0.1'),
+      version='0.1.0',
       description='Python Logging Package for Fybrik Components',
       license='Apache License, Version 2.0',
-      author='Doron Chen',
-      author_email='cdoron@il.ibm.com',
-      url='https://github.com/fybrik/fybrik/tree/master/pkg/logging/python_logging',
+      author='FybrikUser',
+      author_email='FybrikUser@il.ibm.com',
+      url='https://github.com/fybrik/fybrik/tree/master/python/logging',
       packages=['fybrik_python_logging'],
       install_requires=[
           'JSON-log-formatter==0.5.0',
