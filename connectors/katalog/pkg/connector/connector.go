f9b975be7ac89f31f27160042752700996d85ed5
Rohith D Vallam <rovallam@in.ibm.com>
2022-03-12 08:46:57 +0000

    fix golang lint issues
    Signed-off-by: Rohith D Vallam <rovallam@in.ibm.com>

--- connectors/katalog/pkg/connector/connector.go
+++ connectors/katalog/pkg/connector/connector.go
@@ -137,11 +137,13 @@ func (r *Handler) generateUniqueAssetName(namespace string, namePrefix string) (
 	return uniqueAssetName, err
 }
 
-// Enables writing of assets to katalog.
-// The different flows supported are:
-// (a) When DestinationAssetID is specified in the CreateAssetRequest: Then a destination asset id is created with name : <DestinationAssetID>
-// (b) When DestinationAssetID is not specified but ResourceMetadata.Name of source asset is specified in the CreateAssetRequest: Then an asset is created with name: ResourceMetadata.Name-<RANDOMSTRING_LENGTH_4>
-// (c) When DestinationAssetID is not specified and ResourceMetadata.Name of source asset also is not specified: Then an asset is created with name: fybrik-asset-<RANDOMSTRING_LENGTH_4>
+// Enables writing of assets to katalog. The different flows supported are:
+// (a) When DestinationAssetID is specified:
+//     Then a destination asset id is created with name : <DestinationAssetID>
+// (b) When DestinationAssetID is not specified but ResourceMetadata.Name of source asset is specified:
+//     Then an asset is created with name: ResourceMetadata.Name-<RANDOMSTRING_LENGTH_4>
+// (c) When DestinationAssetID and ResourceMetadata.Name of source asset are not specified:
+//     Then an asset is created with name: fybrik-asset-<RANDOMSTRING_LENGTH_4>
 func (r *Handler) createAssetInfo(c *gin.Context) {
 	// Parse request
 	var request datacatalog.CreateAssetRequest
