e42428aa21c688dece82009f9ce7fbd8ae65f020
yingchaodu <92925624+YingchaoDu@users.noreply.github.com>
2022-03-13 14:14:54 +0100

    Add test for validation of infrastructure attributes (#1360)
    * Add test for validation of infrastructure attributes
    
    Signed-off-by: Yingchao Du <yingchao.du@ing.com>

--- pkg/model/attributes/model.go
+++ pkg/model/attributes/model.go
@@ -7,55 +7,6 @@ import (
 	"fybrik.io/fybrik/pkg/model/taxonomy"
 )
 
-// +kubebuilder:validation:Enum=numeric;string;bool
-type AttributeType string
-
-// List of attribute types
-const (
-	Numeric AttributeType = "numeric"
-	String  AttributeType = "string"
-	Bool    AttributeType = "bool"
-)
-
-type RangeType struct {
-	Min int `json:"min,omitempty"`
-	Max int `json:"max,omitempty"`
-}
-
-// +kubebuilder:validation:Enum=fybrikmodule;fybrikstorageaccount;cluster
-type InstanceType string
-
-// List of instance types
-const (
-	Module         InstanceType = "fybrikmodule"
-	Cluster        InstanceType = "cluster"
-	StorageAccount InstanceType = "fybrikstorageaccount"
-)
-
-type InfrastructureElement struct {
-	// Attribute name defined in the taxonomy
-	// +required
-	Attribute taxonomy.Attribute `json:"attribute"`
-	// Description
-	Description string `json:"description,omitempty"`
-	// Attribute type, e.g. numeric or string
-	// +required
-	Type AttributeType `json:"type"`
-	// Attribute value
-	// +required
-	Value string `json:"value"`
-	// Measurement units defined in the tzxonomy
-	Units taxonomy.Units `json:"units,omitempty"`
-	// A resource defined by the attribute ("storageaccount","module","cluster")
-	Object InstanceType `json:"object,omitempty"`
-	// A reference to the resource instance, e.g. storage account name
-	Instance string `json:"instance,omitempty"`
-	// A scale of values (minimum and maximum) when applicable
-	Scale *RangeType `json:"scale,omitempty"`
-	// A list of arguments defining a specific metric, e.g. regions for a bandwidth
-	Arguments []string `json:"arguments,omitempty"`
-}
-
 type Infrastructure struct {
-	Items []InfrastructureElement `json:"infrastructure"`
+	Items []taxonomy.InfrastructureElement `json:"infrastructure"`
 }
--- pkg/model/taxonomy/infrastructure.go
+++ pkg/model/taxonomy/infrastructure.go
@@ -5,3 +5,28 @@ package taxonomy
 
 type Attribute string
 type Units string
+
+// +kubebuilder:validation:Enum=numeric;string;bool
+type AttributeType string
+
+// List of attribute types
+const (
+	Numeric AttributeType = "numeric"
+	String  AttributeType = "string"
+	Bool    AttributeType = "bool"
+)
+
+type RangeType struct {
+	Min int `json:"min,omitempty"`
+	Max int `json:"max,omitempty"`
+}
+
+type InfrastructureElement struct {
+	Attribute   Attribute     `json:"attribute"`
+	Description string        `json:"description,omitempty"`
+	Type        AttributeType `json:"type"`
+	Value       string        `json:"value"`
+	Units       Units         `json:"units,omitempty"`
+	Instance    string        `json:"instance,omitempty"`
+	Scale       *RangeType    `json:"scale,omitempty"`
+}
