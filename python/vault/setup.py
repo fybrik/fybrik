46e2bb098d41fd460673486bafd56a6e24c91f2f
Mohammad-nassar10 <79787844+Mohammad-nassar10@users.noreply.github.com>
2022-05-01 14:01:26 +0300

    Adding fybrik-python-vault library. (#1437)
    * Adding fybrik-python-vault library.
    
    Signed-off-by: mohammad-nassar10 <mohammad.nassar@ibm.com>

---
+++ python/vault/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+import os
+
+setup(name='fybrik_python_vault',
+      version=os.environ.get('VERSION', '0.1.0'),
+      description='Python vault Package for Fybrik Components',
+      license='Apache License, Version 2.0',
+      author='FybrikUser',
+      author_email='FybrikUser@il.ibm.com',
+      url='https://github.com/fybrik/fybrik/tree/master/python/vault',
+      packages=['fybrik_python_vault'],
+      install_requires=[
+          'fybrik_python_logging',
+          'requests'
+      ],
+)
