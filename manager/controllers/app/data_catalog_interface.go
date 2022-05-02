1120574ada8d3f55e49a5f6da2aa15f583669d28
revit13 <eres@il.ibm.com>
2022-05-02 09:40:31 +0300

    Handle new asset registration in a catalog. (#1433)
    * Handle new asset registration in a catalog.
    * Minor fixes.
    * Fix lint errors
    * Fix secret settings.
    * Address review comments in fybrik_notebook_test.go file
    * Address review comments related to createAssetRequest
    * Address review comments.
    * Set Geography when creating new asset.
    * Address comment.
    * Remove left over from testing.
    * Address review comments.
    * Revert setting Geography in metadata of a new asset.
    * Fix rebase issues.
    * Fix lint issue.
    * Return only asset id in the Katalog connector createAsset response.
    * Set Geography in metadata of a new asset.
    * Change credentials in new asset request to include vault address.
    * Generate a new katalog asset name upon creation of a new asset.
    * Minor fix to comment.
    * Remove unused exported vars.
    
    Signed-off-by: Revital Sur <eres@il.ibm.com>
    Co-authored-by: mohammad-nassar10 <mohammad.nassar@ibm.com>

--- manager/controllers/app/data_catalog_interface.go
+++ manager/controllers/app/data_catalog_interface.go
@@ -4,19 +4,61 @@
 package app
 
 import (
-	"errors"
-
 	app "fybrik.io/fybrik/manager/apis/app/v1alpha1"
+	"fybrik.io/fybrik/manager/controllers/utils"
+	"fybrik.io/fybrik/pkg/model/datacatalog"
+	"fybrik.io/fybrik/pkg/vault"
+
+	"github.com/rs/zerolog/log"
 )
 
 // RegisterAsset registers a new asset in the specified catalog
 // Input arguments:
+// - assetID: DataSetID as it appears in fybrik-application
 // - catalogID: the destination catalog identifier
 // - info: connection and credential details
 // Returns:
 // - an error if happened
 // - the new asset identifier
-func (r *FybrikApplicationReconciler) RegisterAsset(catalogID string, info *app.DatasetDetails,
+func (r *FybrikApplicationReconciler) RegisterAsset(assetID string, catalogID string, info *app.DatasetDetails,
 	input *app.FybrikApplication) (string, error) {
-	return "", errors.New("unsupported feature")
+	r.Log.Trace().Msg("RegisterAsset")
+	details := datacatalog.ResourceDetails{}
+	if info.Details != nil {
+		details.Connection = info.Details.Connection
+		details.DataFormat = info.Details.Format
+	}
+
+	var resourceMetadata datacatalog.ResourceMetadata
+	if info.ResourceMetadata != nil {
+		resourceMetadata = *info.ResourceMetadata.DeepCopy()
+	} else {
+		resourceMetadata.Name = assetID
+	}
+	creds := ""
+	if utils.IsVaultEnabled() {
+		creds = utils.GetVaultAddress() + vault.PathForReadingKubeSecret(info.SecretRef.Namespace, info.SecretRef.Name)
+	}
+
+	request := datacatalog.CreateAssetRequest{
+		ResourceMetadata:     resourceMetadata,
+		Details:              details,
+		Credentials:          creds,
+		DestinationCatalogID: catalogID,
+		DestinationAssetID:   assetID,
+	}
+
+	credentialPath := ""
+	if utils.IsVaultEnabled() {
+		credentialPath = utils.GetVaultAddress() + vault.PathForReadingKubeSecret(input.Namespace, input.Spec.SecretRef)
+	}
+
+	var err error
+	var response *datacatalog.CreateAssetResponse
+	if response, err = r.DataCatalog.CreateAsset(&request, credentialPath); err != nil {
+		log.Error().Err(err).Msg("failed to receive the catalog connector response")
+		return "", err
+	}
+
+	return response.AssetID, nil
 }
