1740dedd47edb7c925e1fa8bf9e1711e7aefaedf
Alexey Roytman <roytman@il.ibm.com>
2022-08-14 22:35:08 +0300

    add ls -l tools/bin

--- hack/make-rules/tools.mk
+++ hack/make-rules/tools.mk
@@ -110,15 +110,16 @@ $(TOOLBIN)/json-schema-generator:
 	cd $(TOOLS_DIR); ./install_json-schema-generator.sh
 	$(call post-install-check)
 
-#INSTALL_TOOLS += $(TOOLBIN)/kubeval
-#.PHONY: $(TOOLBIN)/kubeval
-#$(TOOLBIN)/kubeval:
-#	cd $(TOOLS_DIR); ./install_kubeval.sh
-#	$(call post-install-check)
+INSTALL_TOOLS += $(TOOLBIN)/kubeval
+.PHONY: $(TOOLBIN)/kubeval
+$(TOOLBIN)/kubeval:
+	cd $(TOOLS_DIR); ./install_kubeval.sh
+	$(call post-install-check)
 
 .PHONY: install-tools
 install-tools: $(INSTALL_TOOLS)
 	go mod tidy
+	ls -l $(TOOLS_DIR)/bin
 
 .PHONY: uninstall-tools
 uninstall-tools:
