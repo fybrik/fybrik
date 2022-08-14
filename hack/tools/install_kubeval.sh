eb0322a1c6e1a16440bfa44e02682c7fc3ba5bf0
mohammad-nassar10 <mohammad.nassar@ibm.com>
2022-08-14 18:24:30 +0300

    Update install_kubeval.
    Signed-off-by: mohammad-nassar10 <mohammad.nassar@ibm.com>

--- hack/tools/install_kubeval.sh
+++ hack/tools/install_kubeval.sh
@@ -7,12 +7,12 @@ source ./common.sh
 
 
 header_text "Checking for bin/kubeval ${KUBEVAL_VERSION}"
-[[ -f bin/kubeval && `bin/kubeval version | cut -f2 -d" " | head -n1` == ${KUBEVAL_VERSION} ]] && exit 0
+[[ -f bin/kubeval && `bin/kubeval --version | head -n1 | cut -f2 -d" " ` == ${KUBEVAL_VERSION} ]] && exit 0
 
 mkdir -p ./bin
 
-curl -sSLo envtest-bins.tar.gz https://github.com/instrumenta/kubeval/releases/download/v${KUBEVAL_VERSION}/kubeval-linux-amd64.tar.gz
+curl -sSLo envtest-bins.tar.gz https://github.com/instrumenta/kubeval/releases/download/v${KUBEVAL_VERSION}/kubeval-${os}-amd64.tar.gz
 
-tar -zvxf envtest-bins.tar.gz
+tar -zvxf envtest-bins.tar.gz kubeval
 mv kubeval bin
 rm envtest-bins.tar.gz
