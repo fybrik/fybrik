92fde343fd64400e81899aee52641f2629722069
Doron Chen <cdoron@il.ibm.com>
2022-10-13 15:17:49 +0300

    address Alexey's comments
    Signed-off-by: Doron Chen <cdoron@il.ibm.com>

--- third_party/openmetadata/install_OM.sh
+++ third_party/openmetadata/install_OM.sh
@@ -4,8 +4,8 @@ OM_VERSION=0.12.1
 export OPENMETADATA_AIRFLOW_VERSION=0.12.1
 export OPENMETADATA_HELM_CHART_VERSION=0.0.39
 
-export export FYBRIK_BRANCH="${FYBRIK_BRANCH:-master}"
-export export FYBRIK_NAMESPACE="${FYBRIK_NAMESPACE:-fybrik}"
+export FYBRIK_BRANCH="${FYBRIK_BRANCH:-master}"
+export FYBRIK_GITHUB_ORGANIZATION="${FYBRIK_NAMESPACE:-fybrik}"
 
 if [ $# -gt 1 ]; then
     echo "Usage: "$0" [open-metadata-version]"
@@ -30,7 +30,7 @@ cd $tmp_dir
 
 files_to_download=(Makefile Makefile.env pv1.yaml pv2.yaml values-deps.yaml)
 for file in "${files_to_download[@]}"
-    do curl https://raw.githubusercontent.com/${FYBRIK_NAMESPACE}/fybrik/${FYBRIK_BRANCH}/third_party/openmetadata/$file -o $file
+    do curl https://raw.githubusercontent.com/${FYBRIK_GITHUB_ORGANIZATION}/fybrik/${FYBRIK_BRANCH}/third_party/openmetadata/$file -o $file
 done
 
 # install OM + prepare OM for Fybrik
