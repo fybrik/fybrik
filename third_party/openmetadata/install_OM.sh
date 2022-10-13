0378a91a920006a68ebc6c6d7a80e12ddfa788d5
Doron Chen <cdoron@il.ibm.com>
2022-10-13 17:35:12 +0300

    OpenMetadata install script (#1731)
    * install OM script 
    
    Signed-off-by: Doron Chen <cdoron@il.ibm.com>

---
+++ third_party/openmetadata/install_OM.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+OM_VERSION=0.12.1
+export OPENMETADATA_AIRFLOW_VERSION=0.12.1
+export OPENMETADATA_HELM_CHART_VERSION=0.0.39
+
+export FYBRIK_BRANCH="${FYBRIK_BRANCH:-master}"
+export FYBRIK_GITHUB_ORGANIZATION="${FYBRIK_GITHUB_ORGANIZATION:-fybrik}"
+
+if [ $# -gt 1 ]; then
+    echo "Usage: "$0" [open-metadata-version]"
+    exit -1
+fi
+
+if [ $# -eq 1 ]; then
+    OM_VERSION=$1
+fi
+
+supported_om_version=(0.12.1)
+if [[ ! ${supported_om_version[*]} =~ ${OM_VERSION} ]]; then
+    echo supported OM versions are: ${supported_om_version[*]}
+    exit -1
+fi
+
+# create temp directory
+tmp_dir=$(mktemp -d)
+
+# download files to temp directory
+cd $tmp_dir
+
+files_to_download=(Makefile Makefile.env pv1.yaml pv2.yaml values-deps.yaml)
+for file in "${files_to_download[@]}"
+    do curl https://raw.githubusercontent.com/${FYBRIK_GITHUB_ORGANIZATION}/fybrik/${FYBRIK_BRANCH}/third_party/openmetadata/$file -o $file
+done
+
+# install OM + prepare OM for Fybrik
+make
+
+# cleanup
+cd -
+rm -Rf $tmp_dir
