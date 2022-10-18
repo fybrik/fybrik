bf716131ea161c357e3a4144cf838e463fecf6ec
Revital Sur <eres@il.ibm.com>
2022-10-18 06:45:44 +0300

    Add comments to fybrik_python_vault.
    Signed-off-by: Revital Sur <eres@il.ibm.com>

--- python/vault/fybrik_python_vault/__init__.py
+++ python/vault/fybrik_python_vault/__init__.py
@@ -15,23 +15,58 @@ def get_jwt_from_file(file_name):
     with open(file_name) as f:
         return f.read()
 
-def make_http_request(request_type, url, tls_min_version, data=None, headers=None, verify=None, cert=None):
-    """
-    Making http request call.
+def make_http_request(request_type, url, data=None, headers=None,
+                      tls_min_version=None, verify=True, cert=None):
+    """ Making http request call.
     ref: https://requests.readthedocs.io/en/latest/user/advanced/#transport-adapters
+
+    Args:
+        request_type (string): the http request type. For example: "POST" or "GET"
+        url (string): The url.
+        data (dictionary, optional): list of tuples, bytes, or file-like
+        object to send in the body of the request. Defaults to None.
+        headers (dictionary, optional): Dictionary of HTTP Headers to send with the request. Defaults to None.
+        tls_min_version (string, optional): the minimum tls version to use. Defaults to None.
+        verify (optional): Either a boolean, in which case it controls whether we verify
+        the Vault server's TLS certificate, or a string, in which case it must be a path
+        to a CA bundle to use. Defaults to ``True``.
+        cert (tuple, optional): the module ('cert', 'key') pair.
+    Returns:
+        requests.Response
     """
-    # Set properties related to tls when the vault server uses tls.
+
+    # Set tls properties when the vault server uses tls.
     context = create_ssl_context(tls_min_version)
     s = requests.Session()
     s.mount("https://", SSLContextAdapter(context))
  
     req = requests.Request(request_type, url=url, data=data, headers=headers)
     prep = s.prepare_request(req)
-    response = s.send(prep, verify=verify, cert=cert)
+    # Merge environment settings into session
+    settings = s.merge_environment_settings(prep.url, {}, None, verify=verify, cert=cert)
+    response = s.send(prep, **settings)
     return response
 
-def vault_jwt_auth(jwt, vault_address, vault_path, role, datasetID, tls_min_version, verify=None, cert=None):
-    """Authenticate against Vault using a JWT token (i.e., k8s sa token)"""
+def vault_jwt_auth(jwt, vault_address, vault_path, role, datasetID, tls_min_version=None,
+                   verify=True, cert=None):
+    """Authenticate against Vault using a JWT token (i.e., k8s sa token)
+
+    Args:
+        jwt (string): the service account jwt to use to authenticate against Vault.
+        vault_address (string): the Vault address.
+        vault_path (string): the path to authenticate against Vault using Kuberenetes auth method.
+        For example: `/v1/auth/kubernetes/login`.
+        role (string): the name of Vault role.
+        datasetID (string): the dataset ID.
+        tls_min_version (string, optional): the minimum tls version to use. Defaults to None.
+        verify (optional): Either a boolean, in which case it controls whether we verify
+        the Vault server's TLS certificate, or a string, in which case it must be a path
+        to a CA bundle to use. Defaults to ``True``.
+        cert (tuple, optional): the module ('cert', 'key') pair.
+    Returns:
+        the json-encoded content of a response which contains a Vault token, if any
+    """
+
     full_auth_path = vault_address + vault_path
     st = ' '
     if cert:
@@ -40,7 +75,8 @@ def vault_jwt_auth(jwt, vault_address, vault_path, role, datasetID, tls_min_vers
         extra={'full_auth_path': str(full_auth_path),
                DataSetID: datasetID, 'verify': verify, 'cert' : st})
     json = {"jwt": jwt, "role": role}
-    response = make_http_request("POST", full_auth_path, tls_min_version, data=json, verify=verify, cert=cert)
+    response = make_http_request("POST", full_auth_path, data=json, tls_min_version=tls_min_version,
+                                 verify=verify, cert=cert)
     if response.status_code == 200:
         return response.json()
     logger.error("vault authentication failed",
@@ -48,9 +84,27 @@ def vault_jwt_auth(jwt, vault_address, vault_path, role, datasetID, tls_min_vers
                DataSetID: datasetID, ForUser: True})
     return None
 
-def get_raw_secret_from_vault(jwt, secret_path, vault_address, vault_path, role, datasetID, tls_min_version,
-                              verify=None, cert=None):
-    """Get a raw secret from vault by providing a valid jwt token"""
+def get_raw_secret_from_vault(jwt, secret_path, vault_address, vault_path, role, datasetID,
+                              tls_min_version=None, verify=True, cert=None):
+    """Get a raw secret from vault by providing a valid jwt token
+
+    Args:
+        jwt (string): the service account jwt to use to authenticate against Vault.
+        secret_path (string): Vault path of the secret.
+        vault_address (string): the Vault address.
+        vault_path (string): the path to authenticate against Vault using Kuberenetes auth method.
+        For example: `/v1/auth/kubernetes/login`.
+        role (string): the name of Vault role.
+        datasetID (string): the dataset ID.
+        tls_min_version (string, optional): the minimum tls version to use. Defaults to None.
+        verify (optional): Either a boolean, in which case it controls whether we verify
+        the Vault server's TLS certificate, or a string, in which case it must be a path
+        to a CA bundle to use. Defaults to ``True``.
+        cert (tuple, optional): the module ('cert', 'key') pair.
+
+    Returns:
+        the json-encoded content of a response which contains the secret, if any
+    """
     st = ' '
     if cert:
         st = st.join(cert)
@@ -76,7 +130,8 @@ def get_raw_secret_from_vault(jwt, secret_path, vault_address, vault_path, role,
     client_token = vault_auth_response["auth"]["client_token"]
     secret_full_path = vault_address + secret_path
     headers={"X-Vault-Token" : client_token}
-    response = make_http_request("GET", secret_full_path, tls_min_version, headers=headers, verify=verify, cert=cert)
+    response = make_http_request("GET", secret_full_path, headers=headers,
+                                 tls_min_version=tls_min_version, verify=verify, cert=cert)
     logger.debug('Response received from vault when accessing credentials: ' + str(response.status_code),
         extra={'credentials_path': str(secret_full_path),
                DataSetID: datasetID, ForUser: True})
