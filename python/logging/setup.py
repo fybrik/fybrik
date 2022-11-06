29694b322265f1e8984e7e04dd85fbe2b5624697
revit13 <eres@il.ibm.com>
2022-11-06 22:02:18 +0200

    Configure Vault with TLS (#1738)
    * Configure Vault with TLS (#1736)
    * Changes to run-notebook-readflow-tls-tests.
    * Add python TLS package and update python Vault package.
    
    Signed-off-by: Revital Sur <eres@il.ibm.com>

--- python/logging/setup.py
+++ python/logging/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import os
 
 setup(name='fybrik_python_logging',
-      version=os.environ.get('VERSION', '0.1.0'),
+      version=os.environ.get('FYBRIK_PYTHON_LOGGING_VERSION', '0.1.0'),
       description='Python Logging Package for Fybrik Components',
       long_description='## Python logging Package for Fybrik Components',
       long_description_content_type='text/markdown',
