b6194d5d5ad16a965356c7e988c93cef822689e2
ERES@il.ibm.com <eres@m10d.sl.cloud9.ibm.com>
2022-11-15 11:50:36 +0000

    Fix handling of CATALOGED_ASSET env var in notebook sample test runs.
    Signed-off-by: Revital Sur <eres@il.ibm.com>

--- manager/controllers/app/fybrik_notebook_read_flow_test.go
+++ manager/controllers/app/fybrik_notebook_read_flow_test.go
@@ -48,10 +48,7 @@ func TestS3NotebookReadFlow(t *testing.T) {
 		t.Skip("Only executed for notebook tests")
 	}
 	catalogedAsset, ok := os.LookupEnv("CATALOGED_ASSET")
-	if !ok {
-		t.Skip("Error getting CATALOGED_ASSET env var")
-	}
-	if catalogedAsset == "" {
+	if !ok || catalogedAsset == "" {
 		// Use default value which assumes katalog catalog is deployed
 		catalogedAsset = "fybrik-notebook-sample/data-csv"
 	}
