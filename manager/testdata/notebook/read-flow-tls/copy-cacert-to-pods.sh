57b6aac78d6c0b4868fea58e6396a8beb4cb97bc
Revital Sur <eres@il.ibm.com>
2023-03-06 22:25:22 +0200

    Remove option to overwrite base images.
    Signed-off-by: Revital Sur <eres@il.ibm.com>

--- manager/testdata/notebook/read-flow-tls/copy-cacert-to-pods.sh
+++ manager/testdata/notebook/read-flow-tls/copy-cacert-to-pods.sh
@@ -2,6 +2,7 @@
 # Copyright 2021 IBM Corp.
 # SPDX-License-Identifier: Apache-2.0
 
+set -x
 
 kubectl get secret test-tls-ca-certs -n fybrik-system -o json | jq -r '.data."ca.crt"' | base64 -d > ca.crt
 
@@ -11,6 +12,8 @@ export OPENMETADATA_POD=$(kubectl get pods -n fybrik-system |grep openmetadata-c
 kubectl exec -it ${OPENMETADATA_POD} -n fybrik-system -- microdnf install tar
 kubectl cp ca.crt  ${OPENMETADATA_POD}:/etc/ssl/certs/ -n fybrik-system
 export OPA_CONNECTOR_POD=$(kubectl get pods -n fybrik-system |grep opa-connector | awk '{print $1}')
+kubectl exec -it ${OPA_CONNECTOR_POD} -n fybrik-system -- microdnf install tar
 kubectl cp ca.crt ${OPA_CONNECTOR_POD}:/etc/ssl/certs/ -n fybrik-system
 export MANAGER_POD=$(kubectl get pods -n fybrik-system |grep manager | awk '{print $1}')
+kubectl exec -it ${MANAGER_POD} -n fybrik-system -- microdnf install tar
 kubectl cp ca.crt ${MANAGER_POD}:/etc/ssl/certs/ -c manager -n fybrik-system
