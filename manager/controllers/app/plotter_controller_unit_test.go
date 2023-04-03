10e34fa78b67a93e655746e4504bc6a769593692
Shlomit Koyfman <shlomitk@il.ibm.com>
2023-04-03 16:59:07 +0300

    Blueprint changes for isolation (#2053)
    * Use helm Status command with ShowResources == true (#2043)
    
    * changes to helm package
    
    Signed-off-by: Shlomit Koyfman <shlomitk@il.ibm.com>
    
    * network info in blueprints
    
    Signed-off-by: Shlomit Koyfman <shlomitk@il.ibm.com>
    
    * fix typo
    
    Signed-off-by: Shlomit Koyfman <shlomitk@il.ibm.com>
    
    * Call port-forwarding of the module inside a loop in the notebook tests. (#2050)
    
    * Call port-forwarding of the module inside a loop in the notebook tests.
    * Stop port-forward command if fails as suggested by Shlomit.
    * Address review comments.
    * Address review comments.
    * Reduce sleep time and add printings.
    * Rename PortFowardingWaitInSecUntilNextTry.
    
    Signed-off-by: Revital Sur <eres@il.ibm.com>
    
    * populate endpoint boolean indication
    
    Signed-off-by: Shlomit Koyfman <shlomitk@il.ibm.com>
    
    * add test for populating network.endpoint
    
    Signed-off-by: Shlomit Koyfman <shlomitk@il.ibm.com>
    
    * pass cluster of the module along API
    
    Signed-off-by: Shlomit Koyfman <shlomitk@il.ibm.com>
    
    * Add missing flag to opa deployment and update notebook read-flow tests with tls. (#2056)
    
    Signed-off-by: Revital Sur <eres@il.ibm.com>
    
    * Fix golangci-lint timeout error. (#2058)
    
    Signed-off-by: Revital Sur <eres@il.ibm.com>
    
    * populate ingress/egress
    
    Signed-off-by: Shlomit Koyfman <shlomitk@il.ibm.com>
    
    * fix nil reference
    
    Signed-off-by: Shlomit Koyfman <shlomitk@il.ibm.com>
    
    * proper comparison
    
    Signed-off-by: Shlomit Koyfman <shlomitk@il.ibm.com>
    
    * Add kind with calico installation instruction (#2045)
    
    * Add kind with calico installation instruction
    
    Signed-off-by: Alexey Roytman <roytman@il.ibm.com>
    
    * fix deep comparison (#2060)
    
    Signed-off-by: Shlomit Koyfman <shlomitk@il.ibm.com>
    
    * added tests to validate ingress/egress in the chaining example
    
    Signed-off-by: Shlomit Koyfman <shlomitk@il.ibm.com>
    
    * bug fix
    
    Signed-off-by: Shlomit Koyfman <shlomitk@il.ibm.com>
    
    * revert change to plotter
    
    Signed-off-by: Shlomit Koyfman <shlomitk@il.ibm.com>
    
    * add connection urls
    
    Signed-off-by: Shlomit Koyfman <shlomitk@il.ibm.com>
    
    * missing file
    
    Signed-off-by: Shlomit Koyfman <shlomitk@il.ibm.com>
    
    * Comment out depricated linters (#2059)
    
    Signed-off-by: Alexey Roytman <roytman@il.ibm.com>
    
    * pass workload selector to endpoint modules only
    
    Signed-off-by: Shlomit Koyfman <shlomitk@il.ibm.com>
    
    ---------
    
    Signed-off-by: Shlomit Koyfman <shlomitk@il.ibm.com>
    Signed-off-by: Revital Sur <eres@il.ibm.com>
    Signed-off-by: Alexey Roytman <roytman@il.ibm.com>
    Co-authored-by: Revital Sur <eres@il.ibm.com>
    Co-authored-by: Alexey Roytman <roytman@il.ibm.com>

--- manager/controllers/app/plotter_controller_unit_test.go
+++ manager/controllers/app/plotter_controller_unit_test.go
@@ -6,6 +6,7 @@ package app
 import (
 	"context"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/onsi/gomega"
@@ -119,6 +120,8 @@ func TestPlotterController(t *testing.T) {
 	g.Expect(err).To(gomega.BeNil())
 	g.Expect(deployedBp.Spec.Modules["implicit-copy-batch-latest-6575548090"].Chart.Name).
 		To(gomega.Equal("ghcr.io/mesh-for-data/m4d-implicit-copy-batch:0.1.0"))
+	g.Expect(deployedBp.Spec.Modules["implicit-copy-batch-latest-6575548090"].Network.Endpoint).To(gomega.BeFalse())
+	g.Expect(deployedBp.Spec.Modules["arrow-flight-read"].Network.Endpoint).To(gomega.BeTrue())
 	// Check that the auth path of the credentials is set
 	g.Expect(deployedBp.Spec.Modules["implicit-copy-batch-latest-6575548090"].Arguments.Assets[0].Arguments[0].
 		Vault[string(taxonomy.ReadFlow)].AuthPath).To(gomega.Equal("/v1/auth/kubernetes/login"))
@@ -253,4 +256,130 @@ func TestPlotterWithWriteFlow(t *testing.T) {
 		g.Expect(assetState.Ready).To(gomega.BeTrue(), "Asset is ready")
 	}
 	g.Expect(plotter.Status.Assets).To(gomega.HaveLen(2), "Plotter Asset status list contains two elements")
+}
+
+// TestPlotterMultipleAssets checks that the blueprints have been generated correctly.
+// Setup:
+// 3 datasets
+// Main cluster: read (all datasets), transform (dataset1)
+// Remote cluster: transform (dataset2)
+func TestPlotterMultipleAssets(t *testing.T) {
+	t.Parallel()
+	g := gomega.NewGomegaWithT(t)
+	// Set the logger to development mode for verbose logs.
+	logf.SetLogger(zap.New(zap.UseDevMode(true)))
+
+	var (
+		name      = "my-app-default"
+		namespace = environment.GetInternalCRsNamespace()
+	)
+
+	var err error
+	plotterYAML, err := os.ReadFile("../../testdata/plotter-read-transform.yaml")
+	g.Expect(err).To(gomega.BeNil(), "Cannot read plotter file for test")
+	plotter := &fapp.Plotter{}
+	err = yaml.Unmarshal(plotterYAML, plotter)
+	g.Expect(err).To(gomega.BeNil(), "Cannot read plotter file for test")
+
+	plotter.Namespace = namespace
+
+	// Objects to track in the fake client.
+	objs := []runtime.Object{
+		plotter,
+	}
+
+	// Register operator types with the runtime scheme.
+	s := utils.NewScheme(g)
+	// Create a fake client to mock API calls.
+	cl := fake.NewFakeClientWithScheme(s, objs...)
+	dummyManager := dummy.NewDummyClusterManager(
+		make(map[string]*fapp.Blueprint),
+		[]multicluster.Cluster{
+			{
+				Name: "thegreendragon",
+				Metadata: multicluster.ClusterMetadata{
+					Region:        "theshire",
+					VaultAuthPath: "kubernetes",
+				}},
+			{
+				Name: "neverland-cluster",
+				Metadata: multicluster.ClusterMetadata{
+					Region:        "neverland",
+					VaultAuthPath: "kubernetes",
+				}},
+		})
+
+	// Create a PlotterReconciler object with the scheme and fake client.
+	r := &PlotterReconciler{
+		Client:         cl,
+		Log:            logging.LogInit(logging.CONTROLLER, "test-controller"),
+		Scheme:         s,
+		ClusterManager: &dummyManager,
+	}
+
+	// Mock request to simulate Reconcile() being called on an event for a
+	// watched resource .
+	req := reconcile.Request{
+		NamespacedName: types.NamespacedName{
+			Name:      name,
+			Namespace: namespace,
+		},
+	}
+	res, err := r.Reconcile(context.Background(), req)
+	g.Expect(err).To(gomega.BeNil())
+
+	// Check the result of reconciliation to make sure it has the desired state.
+	g.Expect(res.Requeue).To(gomega.BeFalse(), "reconcile did not requeue request as expected")
+
+	// Check if Job has been created and has the correct size.
+	err = cl.Get(context.TODO(), req.NamespacedName, plotter)
+	g.Expect(err).To(gomega.BeNil(), "Can fetch plotter")
+
+	g.Expect(plotter.Status.Blueprints).To(gomega.HaveKey("thegreendragon"))
+	blueprintMeta := plotter.Status.Blueprints["thegreendragon"]
+	g.Expect(blueprintMeta.Name).To(gomega.Equal(plotter.Name))
+	g.Expect(blueprintMeta.Namespace).To(gomega.Equal(plotter.Namespace))
+
+	blueprint := dummyManager.DeployedBlueprints["thegreendragon"]
+	verifiedModules := 0
+	var readInst string
+	for key := range blueprint.Spec.Modules {
+		module := blueprint.Spec.Modules[key]
+		if strings.HasPrefix(key, "airbyte") {
+			readInst = key
+			g.Expect(module.Network.Endpoint).To(gomega.BeTrue())
+			g.Expect(module.Network.Egress).To(gomega.HaveLen(0))
+			g.Expect(module.Network.Ingress).To(gomega.HaveLen(2))
+			g.Expect(module.Network.Ingress[0].Cluster).NotTo(gomega.Equal(module.Network.Ingress[1].Cluster))
+			g.Expect(module.Network.URLs).To(gomega.HaveLen(3))
+			g.Expect(module.Network.URLs).To(gomega.ConsistOf(
+				"https://github.com/Teradata/kylo/raw/master/samples/sample-data/parquet/userdata1.parquet",
+				"https://github.com/Teradata/kylo/raw/master/samples/sample-data/parquet/userdata3.parquet",
+				"https://myserver.com:3000"))
+			verifiedModules += 1
+		} else if strings.HasPrefix(key, "arrow-flight") {
+			g.Expect(module.Network.Endpoint).To(gomega.BeTrue())
+			g.Expect(module.Network.Egress).To(gomega.HaveLen(1))
+			g.Expect(module.Network.Egress[0].Cluster).To(gomega.Equal("thegreendragon"))
+			g.Expect(module.Network.Egress[0].URLs).To(gomega.HaveLen(1))
+			g.Expect(module.Network.Egress[0].URLs[0]).To(gomega.Equal("my-app-fybrik-blueprints-airbyte-module.fybrik-blueprints:80"))
+			g.Expect(module.Network.Ingress).To(gomega.HaveLen(0))
+			verifiedModules += 1
+		}
+	}
+	g.Expect(verifiedModules).To(gomega.Equal(2))
+	blueprint = dummyManager.DeployedBlueprints["neverland-cluster"]
+	g.Expect(blueprint.Spec.Modules).To(gomega.HaveLen(1))
+	for key := range blueprint.Spec.Modules {
+		g.Expect(key).To(gomega.HavePrefix("arrow-flight"))
+		module := blueprint.Spec.Modules[key]
+		g.Expect(module.Network.Endpoint).To(gomega.BeTrue())
+		g.Expect(module.Network.Egress).To(gomega.HaveLen(1))
+		readRelease := utils.GetReleaseName(plotter.Labels[utils.ApplicationNameLabel],
+			plotter.Annotations[utils.FybrikAppUUID], readInst)
+		g.Expect(module.Network.Egress[0].Release).To(gomega.Equal(readRelease))
+		g.Expect(module.Network.Egress[0].Cluster).To(gomega.Equal("thegreendragon"))
+		g.Expect(module.Network.Ingress).To(gomega.HaveLen(0))
+		verifiedModules += 1
+	}
 }
